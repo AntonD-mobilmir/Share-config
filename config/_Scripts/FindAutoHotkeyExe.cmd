37af9b6d8cce4ee6ae7adfb47b2ede14d4a88d05
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2016-11-08 14:24:40 +0300

    FindAutoHotkeyExe.cmd немного переписан: теперь использует find_exe.cmd.
    Для отключения интерактивного режима, передаются параметры «/ErrorStdOut .».
    Без них, AutoHotkey.exe при пробном запуске запускает AutoHotkey.ahk, открывает справку или выводит сообщение:
    Script file not found:
    D:\Users\*\Documents\AutoHotkey.ahk

--- config/_Scripts/FindAutoHotkeyExe.cmd
+++ config/_Scripts/FindAutoHotkeyExe.cmd
@@ -1,24 +1,44 @@
-@REM coding:CP866
-@REM by LogicDaemon <www.logicdaemon.ru>
-@REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
-
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
 FOR /F "usebackq tokens=2 delims==" %%I IN (`ftype AutoHotkeyScript`) DO CALL :GetFirstArg AutohotkeyExe %%I
-GOTO :SkipGetFirstArg
-:GetFirstArg
-    SET %1=%2
+)
+(
+    CALL :CheckAutohotkeyExe && EXIT /B
+    rem continuing here if AutoHotkeyScript type isn't defined or specified path points to incorect location
+    SET AutohotkeyExe="%ProgramFiles%\AutoHotkey\AutoHotkey.exe"
+    CALL :CheckAutohotkeyExe && EXIT /B
+    SET AutohotkeyExe="%ProgramFiles(x86)%\AutoHotkey\AutoHotkey.exe"
+    CALL :CheckAutohotkeyExe && EXIT /B
+    CALL :tryutilsdir
+    CALL :CheckAutohotkeyExe && EXIT /B
+
+    REM without these options, autohotkey.exe starts AutoHotkey.ahk, opens help or says
+    rem 	Script file not found:
+    rem 	D:\Users\*\Documents\AutoHotkey.ahk
+    SET "findExeTestExecutionOptions=/ErrorStdOut ."
+    CALL "%~dp0find_exe.cmd" AutohotkeyExe AutoHotkey.exe
+    REM explicit backup not needed in same scope
+    SET "findExeTestExecutionOptions=%findExeTestExecutionOptions%"
 EXIT /B
-:SkipGetFirstArg
-
-IF DEFINED AutohotkeyExe IF EXIST %AutohotkeyExe% EXIT /B 0
-rem continuing here if AutoHotkeyScript isn't defined or specified path points to incorect location
-
-SET AutohotkeyExe="%ProgramFiles%\AutoHotkey\AutoHotkey.exe"
-IF NOT EXIST %AutohotkeyExe% SET AutohotkeyExe="%ProgramFiles(x86)%\AutoHotkey\AutoHotkey.exe"
-IF NOT EXIST %AutohotkeyExe% CALL :tryutilsdir
+)
+:CheckAutohotkeyExe
+(
+IF NOT DEFINED AutohotkeyExe EXIT /B 1
 IF NOT EXIST %AutohotkeyExe% EXIT /B 1
 EXIT /B 0
-
+)
 :tryutilsdir
+(
     IF NOT DEFINED utilsdir CALL "%~dp0FindSoftwareSource.cmd"
+    IF NOT DEFINED utilsdir EXIT /B 1
+)
+(
     SET AutohotkeyExe="%utilsdir%AutoHotkey.exe"
 EXIT /B
+)
+:GetFirstArg
+(
+    SET %1=%2
+EXIT /B
+)
