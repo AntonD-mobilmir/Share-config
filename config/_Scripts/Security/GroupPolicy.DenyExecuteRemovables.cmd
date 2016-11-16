ee9a750ec2beb57b7860cb3c50d65be8f799faf7
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2016-11-16 10:59:24 +0300

    GroupPolicy.DenyExecuteRemovables.cmd теперь выбирает архив с групповой политикой, специфичной для ОС, на которой он работает (если есть), иначе использует общий.
    Добавлена ГП для Windows 7, потому что ГП от Windows 10 не работала.

--- config/_Scripts/Security/GroupPolicy.DenyExecuteRemovables.cmd
+++ config/_Scripts/Security/GroupPolicy.DenyExecuteRemovables.cmd
@@ -5,12 +5,24 @@ SETLOCAL ENABLEEXTENSIONS
 IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
 IF NOT DEFINED PROGRAMDATA SET "PROGRAMDATA=%ALLUSERSPROFILE%\Application Data"
 IF NOT DEFINED APPDATA IF EXIST "%USERPROFILE%\Application Data" SET "APPDATA=%USERPROFILE%\Application Data"
+
+SET "policyArchivePrefix=%~dp0GroupPolicy.DenyExecuteRemovables"
 )
 (
 CALL "%~dp0..\find7zexe.cmd"
 SET "System32=%SystemRoot%\System32"
 IF EXIST "%SystemRoot%\SysNative\*.*" SET "System32=%SystemRoot%\SysNative"
 rem via https://social.technet.microsoft.com/Forums/windows/en-US/2030e62e-a60d-442a-8455-647b569caffb/export-import-local-group-policy-objects?forum=itproxpsp
+
+CALL "%~dp0..\CheckWinVer.cmd" 10 || CALL :SetPolicyArchiveSuffix
 )
-%exe7z% x -aoa -x!gpt.ini -o"%System32%\GroupPolicy" "%~dp0GroupPolicy.DenyExecuteRemovables.7z"
+(
+%exe7z% x -aoa -x!gpt.ini -o"%System32%\GroupPolicy" "%policyArchivePrefix%%policyArchiveSuffix%.7z" || EXIT /B
 "%System32%\gpupdate.exe" /force
+EXIT /B
+)
+:SetPolicyArchiveSuffix
+(
+IF EXIST "%policyArchivePrefix%.%WinVerNum:~0,3%.7z" SET "policyArchiveSuffix=.%WinVerNum:~0,3%"
+EXIT /B
+)
