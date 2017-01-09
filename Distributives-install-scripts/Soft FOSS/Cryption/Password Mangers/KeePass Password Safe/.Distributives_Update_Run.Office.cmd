b7c72f93209ae776ad7143a57de934685435cd68
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-01-09 18:21:30 +0300

    мелкие изменения

--- Distributives-install-scripts/Soft FOSS/Cryption/Password Mangers/KeePass Password Safe/.Distributives_Update_Run.Office.cmd
+++ Distributives-install-scripts/Soft FOSS/Cryption/Password Mangers/KeePass Password Safe/.Distributives_Update_Run.Office.cmd
@@ -1,2 +1,7 @@
-@REM coding:OEM
-CALL "%~dp0download.cmd" %*
+@(REM coding:OEM
+SET "srcpath=%~dp0"
+SET "distcleanup=1"
+IF NOT DEFINED baseScripts SET "baseScripts=\Scripts"
+rem Always points to latest, e.g. 2.x version
+)
+CALL "%baseScripts%\_DistDownload.cmd" http://sourceforge.net/projects/keepass/files/latest/download KeePass-*.zip -N -A.zip
--- Distributives-install-scripts/Soft FOSS/Cryption/Password Mangers/KeePass Password Safe/download.cmd
+++
@@ -1,7 +0,0 @@
-@REM coding:OEM
-SET srcpath=%~dp0
-SET distcleanup=1
-
-rem Always points to latest, e.g. 2.x version
-IF NOT DEFINED baseScripts SET baseScripts=\Scripts
-CALL "%baseScripts%\_DistDownload.cmd" http://sourceforge.net/projects/keepass/files/latest/download KeePass-*.zip -N -A.zip
