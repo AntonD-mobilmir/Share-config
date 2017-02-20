4bc8d3a0494e2cfdf2d4aca399bafe41208ae85c
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-02-20 12:15:09 +0300

    backup_1S.cmd:
    * оптимизирован
    * в журнал запуска будет писаться более подробная информация
    * в оба журнала будет записываться и время перед строкой с текстом
    * 7-zip будет запускаться без START ""
    * архив будет создаваться с расширением tmp, и затем переименовываться в случае успешной архивации

--- Rarus_ShopBTS_InitialBase/Rarus_Scripts/backup_1S.cmd
+++ Rarus_ShopBTS_InitialBase/Rarus_Scripts/backup_1S.cmd
@@ -4,48 +4,55 @@ REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 In
 SETLOCAL ENABLEEXTENSIONS
 CALL :Find7z || GOTO :EXITWITHERROR
 IF NOT DEFINED destdir CALL "%ProgramData%\mobilmir.ru\_rarus_backup_get_files.cmd" || CALL "%SystemDrive%\Local_Scripts\_rarus_backup_get_files.cmd"
-SET Switches7z=-mqs=on
+    SET "Switches7z=-mqs=on"
 rem SET Switches7z=-m0=LZMA2:a=1:d22:fb=64
+    SET "datem=%DATE:~-4,4%-%DATE:~-7,2%"
+    SET "dated=%DATE:~-4,4%-%DATE:~-7,2%-%DATE:~-10,2%"
+    FOR /F "usebackq tokens=2 delims==" %%I IN (`FTYPE AutoHotkeyScript`) DO SET ahkexe=%%I
 )
-IF NOT EXIST %destdir% GOTO :NODESTINATION
-ECHO. >"%rarusbackupflag%"
-SET datem=%DATE:~-4,4%-%DATE:~-7,2%
-SET dated=%DATE:~-4,4%-%DATE:~-7,2%-%DATE:~-10,2%
-
-ECHO.>>"%rarusbackuplogfile%"
-ECHO Archiving of %CD% started %DATE% %TIME%>>"%rarusbackuplogfile%"
-
-SET destfname=ShopBTS_%datem%.7z
-SET destdname=ShopBTS_%dated%.7z
-
-FOR /F "usebackq tokens=2 delims==" %%I IN (`FTYPE AutoHotkeyScript`) DO SET ahkexe=%%I
-CALL :Get1stToken ahkexe %ahkexe%
-%ahkexe% /ErrorStdOut "%~dp0backup_1S_RemoveOld.ahk" >>"%rarusbackuplogfile%" 2>&1
-
-REM Full archive monthly & differential daily
-IF EXIST "%destdir%\%destfname%" (
-    REM differential archiving if archive dont exist
-    IF NOT EXIST "%destdir%\%destdname%" (
-	ECHO Making differential archive>>"%rarusbackuplogfile%"
-	START "" /B /WAIT %exe7z% a -r -slp -bd %Switches7z% -u- -up0q3z0!"%destdir%\%destdname%" -x!Backup -x!Exchange -x!NEW_STRU -x!SYSLOG -xr!*.rar -xr!*.7z -xr!*.LCK -xr!*.cdx -x!*.flag "%destdir%\%destfname%" *>>"%rarusbackuplogfile%" 2>&1
-	IF ERRORLEVEL 2 GOTO :EXITWITHERROR
-	DEL "%rarusbackupflag%"
-	START "" /B /WAIT %exe7z% t -bd "%destdir%\%destdname%">>"%rarusbackuplogfile%" 2>&1
-	IF ERRORLEVEL 2 GOTO :EXITWITHERROR
+(
+    IF NOT EXIST "%destdir%" GOTO :NODESTINATION
+    SET "destfname=ShopBTS_%datem%.7z"
+    SET "destdname=ShopBTS_%dated%.7z"
+    (ECHO.) >"%rarusbackupflag%"
+    (
+    ECHO.
+    ECHO %DATE% %TIME% Starting archiving of "%CD%"
+    )>>"%rarusbackuplogfile%"
+    CALL :Get1stToken ahkexe %ahkexe%
+)
+(
+    %ahkexe% /ErrorStdOut "%~dp0backup_1S_RemoveOld.ahk" >>"%rarusbackuplogfile%" 2>&1
+    REM Full archive monthly & differential daily
+    IF EXIST "%destdir%\%destfname%" (
+	REM differential archiving if archive dont exist
+	IF NOT EXIST "%destdir%\%destdname%" (
+	    ECHO %DATE% %TIME% Making differential archive
+	    ECHO %DATE% %TIME% Making differential archive>>"%rarusbackuplogfile%"
+	    %exe7z% a -r -slp -bd %Switches7z% -u- -up0q3z0!"%destdir%\%destdname%.tmp" -x!Backup -x!Exchange -x!NEW_STRU -x!SYSLOG -xr!*.rar -xr!*.7z -xr!*.LCK -xr!*.cdx -x!*.flag -- "%destdir%\%destfname%" *>>"%rarusbackuplogfile%" 2>&1
+	    IF ERRORLEVEL 2 GOTO :EXITWITHERROR
+	    REN "%destdir%\%destdname%.tmp" *.
+	    DEL "%rarusbackupflag%"
+	    %exe7z% t -bd -- "%destdir%\%destdname%" >>"%rarusbackuplogfile%" 2>&1
+	    IF ERRORLEVEL 2 GOTO :EXITWITHERROR
+	) ELSE (
+	    ECHO %DATE% %TIME% "%destdir%\%destdname%" already exist
+	    ECHO %DATE% %TIME% "%destdir%\%destdname%" already exist>>"%rarusbackuplogfile%"
+	    GOTO :EXITWITHERROR
+	)
     ) ELSE (
-	ECHO "%destdir%\%destdname%" already exist>>"%rarusbackuplogfile%"
-	GOTO :EXITWITHERROR
+	ECHO %DATE% %TIME% Making full archive
+	ECHO %DATE% %TIME% Making full archive>>"%rarusbackuplogfile%"
+	%exe7z% a -r -slp -bd %Switches7z% -x!Exchange -x!NEW_STRU -xr!*.rar -xr!*.7z -xr!*.LCK -xr!*.cdx -x!*.flag -- "%destdir%\%destfname%.tmp" *>>"%rarusbackuplogfile%" 2>&1
+	IF ERRORLEVEL 2 GOTO :EXITWITHERROR
+	REN "%destdir%\%destfname%.tmp" *.
+	DEL "%rarusbackupflag%"
+	%exe7z% t -bd -- "%destdir%\%destfname%" >>"%rarusbackuplogfile%" 2>&1
+	IF ERRORLEVEL 2 GOTO :EXITWITHERROR
     )
-) ELSE (
-    ECHO Making full archive>>"%rarusbackuplogfile%"
-    START "" /B /WAIT %exe7z% a -r -slp -bd %Switches7z% -x!Exchange -x!NEW_STRU -xr!*.rar -xr!*.7z -xr!*.LCK -xr!*.cdx -x!*.flag "%destdir%\%destfname%" *>>"%rarusbackuplogfile%" 2>&1
-    IF ERRORLEVEL 2 GOTO :EXITWITHERROR
-    DEL "%rarusbackupflag%"
-    START "" /B /WAIT %exe7z% t -bd "%destdir%\%destfname%">>"%rarusbackuplogfile%" 2>&1
-    IF ERRORLEVEL 2 GOTO :EXITWITHERROR
-)
 
-DEL "%rarusbackupflag%"
+    DEL "%rarusbackupflag%"
+)
 EXIT /B %ExitErrorLevel%
 
 :Find7z
@@ -59,12 +66,16 @@ EXIT /B %ExitErrorLevel%
 EXIT /B
 
 :NODESTINATION
-    ECHO %destdir% does not exist>>"%rarusbackuplogfile%"
+(
+    ECHO "%destdir%" does not exist
+    ECHO "%destdir%" does not exist>>"%rarusbackuplogfile%"
+)
 :EXITWITHERROR
 (
     DEL "%rarusbackupflag%"
-    ECHO Backup process terminated>>"%rarusbackuplogfile%"
-    SET ExitErrorLevel=1
+    ECHO %DATE% TIME% Backup process terminated
+    ECHO %DATE% TIME% Backup process terminated>>"%rarusbackuplogfile%"
+    SET "ExitErrorLevel=1"
 EXIT /B
 )
 :Get1stToken
