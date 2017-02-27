a5f649939df208b55dcbc6455b94489587f598da
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-02-27 21:08:52 +0300

    в +notebook.cmd отключена установка Quick Config

--- config/+notebook.cmd
+++ config/+notebook.cmd
@@ -7,5 +7,5 @@ IF DEFINED PROCESSOR_ARCHITEW6432 (
 POWERCFG /HIBERNATE /SIZE 40
 
 IF NOT DEFINED SoftSourceDir CALL "%~dp0_Scripts\FindSoftwareSource.cmd"
-IF NOT DEFINED instQuickConfig SET /P "instQuickConfig=Установить Intelloware Quick Config? [1=да, остальное=нет]"
-IF "%instQuickConfig%"=="1" START "Установка Intelloware Quick Config" /WAIT /I msiexec.exe /i "%SoftSourceDir%\Network\Configuration\Intelloware Quick Config\QuickConfig.msi" /q
+rem IF NOT DEFINED instQuickConfig SET /P "instQuickConfig=Установить Intelloware Quick Config? [1=да, остальное=нет]"
+rem IF "%instQuickConfig%"=="1" START "Установка Intelloware Quick Config" /WAIT /I msiexec.exe /i "%SoftSourceDir%\Network\Configuration\Intelloware Quick Config\QuickConfig.msi" /q
