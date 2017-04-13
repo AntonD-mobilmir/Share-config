4fc6086f57accc42c87240971bd424ae2d138fa2
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-04-13 13:39:02 +0300

    скрипт установки Mozilla Thunderbird теперь не будет удалять папку Mozilla Thunderbird\distribution\extensions после установки, если до установки она существовала

--- Distributives-install-scripts/Soft/Network/Mail News/Mozilla Thunderbird/install.cmd
+++ Distributives-install-scripts/Soft/Network/Mail News/Mozilla Thunderbird/install.cmd
@@ -11,13 +11,13 @@ CALL "%ProgramData%\mobilmir.ru\_get_defaultconfig_source.cmd"
 SET "MozMainSvcUninst=%lProgramFiles%\Mozilla Maintenance Service\Uninstall.exe"
 CALL "%srcpath%find_distributive.cmd" || EXIT /B
 IF NOT DEFINED distFullPath EXIT /B 1
+IF EXIST "%lProgramFiles%\Mozilla Thunderbird\distribution\extensions" SET "KeepExtensions=1"
 )
 "%distFullPath%" /INI="%srcpath%install.ini"
 (
-
-IF ERRORLEVEL 1 SET "ErrorMemory=%ERRORLEVEL%"
+SET "ErrorMemory=%ERRORLEVEL%"
 REM Default extensions
-RD /S /Q "%lProgramFiles%\Mozilla Thunderbird\distribution\extensions"
+IF NOT DEFINED KeepExtensions RD /S /Q "%lProgramFiles%\Mozilla Thunderbird\distribution\extensions"
 REM Update Service
 IF EXIST "%MozMainSvcUninst%" "%MozMainSvcUninst%" /S
 )
