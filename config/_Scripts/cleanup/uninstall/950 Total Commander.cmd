7f600a61fb03b22de846bb3e5ba94b3fd4f22193
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-04-20 13:40:38 +0300

    uninstall/950 Total Commander.cmd немного оптимизирован (больше команд включено в блок ( ))

--- config/_Scripts/cleanup/uninstall/950 Total Commander.cmd
+++ config/_Scripts/cleanup/uninstall/950 Total Commander.cmd
@@ -1,16 +1,16 @@
 @(REM coding:CP866
 REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
-)
 
-CALL :RemoveCheckKill "%lProgramFiles%\Total Commander" totalcmd.exe
-FOR /D %%I IN (%SystemDrive%\Users\*) DO (
-    CALL :RemoveCheckKill "%%~I\Program Files\Total Commander" totalcmd.exe
-    CALL :RemoveCheckKill "%%~I\AppData\Local\Programs\Total Commander" totalcmd.exe
+    CALL :RemoveCheckKill "%lProgramFiles%\Total Commander" totalcmd.exe
+    FOR /D %%I IN (%SystemDrive%\Users\*) DO (
+	CALL :RemoveCheckKill "%%~I\Program Files\Total Commander" totalcmd.exe
+	CALL :RemoveCheckKill "%%~I\AppData\Local\Programs\Total Commander" totalcmd.exe
+    )
+    EXIT /B
 )
-EXIT /B
-
 :RemoveCheckKill <dir> <executable>
+(
     RD /S /Q "%~1"
     IF EXIST "%~1" (
 	%SystemRoot%\System32\taskkill.exe /F /IM %2
@@ -18,3 +18,4 @@ EXIT /B
 	GOTO :RemoveCheckKill
     )
 EXIT /B
+)
