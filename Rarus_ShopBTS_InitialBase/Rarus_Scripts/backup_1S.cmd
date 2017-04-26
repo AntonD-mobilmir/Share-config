3120b9f8e3e1bb21d7e6a92e2381991787e64aa6
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-04-26 13:13:59 +0300

    backup_1S.cmd будет использовать zpaq[64].exe, если он будет найден в системе. Архивы будут создаваться по названию года.
    Если zpaq не найдётся, как и раньше будет использоваться 7-zip.

--- Rarus_ShopBTS_InitialBase/Rarus_Scripts/backup_1S.cmd
+++ Rarus_ShopBTS_InitialBase/Rarus_Scripts/backup_1S.cmd
@@ -2,81 +2,121 @@
 REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
 SETLOCAL ENABLEEXTENSIONS
-CALL :Find7z || GOTO :EXITWITHERROR
-IF NOT DEFINED destdir CALL "%ProgramData%\mobilmir.ru\_rarus_backup_get_files.cmd" || CALL "%SystemDrive%\Local_Scripts\_rarus_backup_get_files.cmd"
-    SET "Switches7z=-mqs=on"
-rem SET Switches7z=-m0=LZMA2:a=1:d22:fb=64
+IF NOT DEFINED ProgramData SET "ProgramData=%ALLUSERSPROFILE%\Application Data"
+IF NOT DEFINED DefaultsSource CALL "%ProgramData%\mobilmir.ru\_get_defaultconfig_source.cmd" || CALL "%SystemDrive%\Local_Scripts\_get_defaultconfig_source.cmd"
+
+    SET "srcdir=%~1"
+    IF NOT DEFINED srcdir SET "srcdir=D:\1S\Rarus\ShopBTS"
+    
+    IF NOT DEFINED destdir CALL "%ProgramData%\mobilmir.ru\_rarus_backup_get_files.cmd" || CALL "%SystemDrive%\Local_Scripts\_rarus_backup_get_files.cmd"
+    CALL :Findzpaq || CALL :Find7z || GOTO :ExitWithError
+    SET "Switches7z=-slp -bd -mqs=on"
+    rem -m0=LZMA2:a=1:d22:fb=64
+    SET "datey=%DATE:~-4,4%"
     SET "datem=%DATE:~-4,4%-%DATE:~-7,2%"
     SET "dated=%DATE:~-4,4%-%DATE:~-7,2%-%DATE:~-10,2%"
+
+    SET "exclusionszpaq=-not Backup -not Exchange -not ExtForms/Scripts -not ExtForms/post -not NEW_STRU -not Shared -not *.rar -not *.7z -not *.LCK -not *.cdx -not !*.flag -not sendEmail* -not zpaq*"
+    SET "exclusions7z=-x!Backup -x!Exchange -x!ExtForms/Scripts -x!ExtForms/post -x!NEW_STRU -x!Shared -xr!*.rar -xr!*.7z -xr!*.LCK -xr!*.cdx -x!*.flag -x!sendEmail* -x!zpaq*"
+
     FOR /F "usebackq tokens=2 delims==" %%I IN (`FTYPE AutoHotkeyScript`) DO SET ahkexe=%%I
 )
 (
-    IF NOT EXIST "%destdir%" GOTO :NODESTINATION
+    CALL :Get1stToken ahkexe %ahkexe%
+    
+    PUSHD "%srcdir%" || (ECHO Не удалось перейти в исходную папку & EXIT /B)
+    IF NOT EXIST "%destdir%" CALL :ExitWithError "%destdir%" "не существует" & EXIT /B
     SET "destfname=ShopBTS_%datem%.7z"
     SET "destdname=ShopBTS_%dated%.7z"
     (ECHO.) >"%rarusbackupflag%"
     (
     ECHO.
-    ECHO %DATE% %TIME% Starting archiving of "%CD%"
+    ECHO %DATE% %TIME% Starting archiving of "%srcdir%"
     )>>"%rarusbackuplogfile%"
-    CALL :Get1stToken ahkexe %ahkexe%
 )
 (
     %ahkexe% /ErrorStdOut "%~dp0backup_1S_RemoveOld.ahk" >>"%rarusbackuplogfile%" 2>&1
-    REM Full archive monthly & differential daily
-    IF EXIST "%destdir%\%destfname%" (
-	REM differential archiving if archive dont exist
-	IF NOT EXIST "%destdir%\%destdname%" (
-	    ECHO %DATE% %TIME% Making differential archive
-	    ECHO %DATE% %TIME% Making differential archive>>"%rarusbackuplogfile%"
-	    %exe7z% a -r -slp -bd %Switches7z% -u- -up0q3z0!"%destdir%\%destdname%.tmp" -x!Backup -x!Exchange -x!NEW_STRU -x!SYSLOG -xr!*.rar -xr!*.7z -xr!*.LCK -xr!*.cdx -x!*.flag -- "%destdir%\%destfname%" *>>"%rarusbackuplogfile%" 2>&1
-	    IF ERRORLEVEL 2 GOTO :EXITWITHERROR
-	    REN "%destdir%\%destdname%.tmp" *.
-	    DEL "%rarusbackupflag%"
-	    %exe7z% t -bd -- "%destdir%\%destdname%" >>"%rarusbackuplogfile%" 2>&1
-	    IF ERRORLEVEL 2 GOTO :EXITWITHERROR
-	) ELSE (
-	    ECHO %DATE% %TIME% "%destdir%\%destdname%" already exist
-	    ECHO %DATE% %TIME% "%destdir%\%destdname%" already exist>>"%rarusbackuplogfile%"
-	    GOTO :EXITWITHERROR
-	)
+    IF DEFINED zpaqexe (
+	START "zpaq a" /B /WAIT /NORMAL %zpaqexe% a "%destdir%\%datey%.zpaq" * -m34 %exclusionszpaq% >>"%rarusbackuplogfile%" 2>>"%rarusbackupzpaqerrfile%"
+	CALL :DelBackupFlag
     ) ELSE (
-	ECHO %DATE% %TIME% Making full archive
-	ECHO %DATE% %TIME% Making full archive>>"%rarusbackuplogfile%"
-	%exe7z% a -r -slp -bd %Switches7z% -x!Exchange -x!NEW_STRU -xr!*.rar -xr!*.7z -xr!*.LCK -xr!*.cdx -x!*.flag -- "%destdir%\%destfname%.tmp" *>>"%rarusbackuplogfile%" 2>&1
-	IF ERRORLEVEL 2 GOTO :EXITWITHERROR
-	REN "%destdir%\%destfname%.tmp" *.
-	DEL "%rarusbackupflag%"
-	%exe7z% t -bd -- "%destdir%\%destfname%" >>"%rarusbackuplogfile%" 2>&1
-	IF ERRORLEVEL 2 GOTO :EXITWITHERROR
+	REM Full archive monthly & differential daily
+	IF EXIST "%destdir%\%destfname%" (
+	    REM differential archiving if archive dont exist
+	    IF NOT EXIST "%destdir%\%destdname%" (
+		ECHO %DATE% %TIME% Making differential archive
+		ECHO %DATE% %TIME% Making differential archive>>"%rarusbackuplogfile%"
+		START "7z a diff" /B /WAIT /NORMAL %exe7z% a -r %Switches7z% -u- -up0q3z0!"%destdir%\%destdname%.tmp" %exclusions7z% -x!SYSLOG -- "%destdir%\%destfname%" *>>"%rarusbackuplogfile%" 2>&1
+		IF ERRORLEVEL 2 CALL :ExitWithError "при создании дифференциального архива" "%destdir%\%destdname%.tmp" & EXIT /B
+		REN "%destdir%\%destdname%.tmp" *.
+		CALL :DelBackupFlag
+		%exe7z% t -bd -- "%destdir%\%destdname%" >>"%rarusbackuplogfile%" 2>&1
+		IF ERRORLEVEL 2 CALL :ExitWithError "при тестировании дифференциального архива" "%destdir%\%destdname%" & EXIT /B
+	    ) ELSE (
+		ECHO %DATE% %TIME% "%destdir%\%destdname%" already exist
+		ECHO %DATE% %TIME% "%destdir%\%destdname%" already exist>>"%rarusbackuplogfile%"
+		GOTO :ExitWithError
+	    )
+	) ELSE (
+	    ECHO %DATE% %TIME% Making full archive
+	    ECHO %DATE% %TIME% Making full archive>>"%rarusbackuplogfile%"
+	    START "7z a full" /B /WAIT /NORMAL %exe7z% a -r %Switches7z% %exclusions7z% -- "%destdir%\%destfname%.tmp" *>>"%rarusbackuplogfile%" 2>&1
+	    IF ERRORLEVEL 2 CALL :ExitWithError "При создании полного архива" "%destdir%\%destfname%.tmp" & EXIT /B
+	    CALL :DelBackupFlag
+	    %exe7z% t -bd -- "%destdir%\%destfname%" >>"%rarusbackuplogfile%" 2>&1
+	    IF ERRORLEVEL 2 CALL :ExitWithError "При тестировании полного архива" "%destdir%\%destfname%.tmp" & EXIT /B
+	    REN "%destdir%\%destfname%.tmp" *.
+	)
     )
-
-    DEL "%rarusbackupflag%"
-)
-EXIT /B %ExitErrorLevel%
-
-:Find7z
-    IF NOT DEFINED ProgramData SET "ProgramData=%ALLUSERSPROFILE%\Application Data"
-    IF NOT DEFINED DefaultsSource CALL "%ProgramData%\mobilmir.ru\_get_defaultconfig_source.cmd" || CALL "%SystemDrive%\Local_Scripts\_get_defaultconfig_source.cmd"
-    CALL :GetDir configDir %DefaultsSource%
-    CALL "%configDir%_Scripts\find7zexe.cmd" || CALL "%configDir%_Scripts\find_exe.cmd" exe7z "%configDir%..\Soft\PreInstalled\utils\7za.exe" || (
-	ECHO [!!!] Could not find 7-Zip!>>"%rarusbackuplogfile%"
-	EXIT /B 1
-    )
+    
+    CALL :DelBackupFlag
+    POPD
 EXIT /B
-
-:NODESTINATION
-(
-    ECHO "%destdir%" does not exist
-    ECHO "%destdir%" does not exist>>"%rarusbackuplogfile%"
 )
-:EXITWITHERROR
+:Findzpaq
+SET "zpaqpath=%ProgramFiles%\zpaq"
 (
-    DEL "%rarusbackupflag%"
+    IF /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" SET zpaqexe="%zpaqpath%\zpaq64.exe"
+    IF DEFINED PROCESSOR_ARCHITEW6432 SET zpaqexe="%zpaqpath%\zpaq64.exe"
+    IF NOT DEFINED zpaqexe SET zpaqexe="%zpaqpath%\zpaq.exe"
+)
+(
+    IF EXIST %zpaqexe% EXIT /B 0
+    SET "zpaqexe="
+    EXIT /B 1
+)
+:Find7z
+    IF NOT DEFINED configDir CALL :GetDir configDir %DefaultsSource%
+(
+    CALL "%configDir%_Scripts\find7zexe.cmd" || CALL "%configDir%_Scripts\find_exe.cmd" exe7z "%configDir%..\Soft\PreInstalled\utils\7za.exe" || CALL :LogError "Не найден 7-Zip"
+EXIT /B
+)
+:ExitWithError <text>
+(
+    IF ERRORLEVEL 1 (
+	SET "ExitErrorLevel=%ERRORLEVEL%"
+    ) ELSE (
+	SET "ExitErrorLevel=1
+    )
+    IF NOT "%~1"=="" CALL :LogError %*
+    CALL :DelBackupFlag
     ECHO %DATE% %TIME% Backup process terminated
     ECHO %DATE% %TIME% Backup process terminated>>"%rarusbackuplogfile%"
-    SET "ExitErrorLevel=1"
-EXIT /B
+    POPD
+)
+EXIT %ExitErrorLevel%
+:DelBackupFlag
+(
+    ECHO Удаление флага архивации "%rarusbackupflag%"
+    DEL "%rarusbackupflag%" || CALL :LogError при удалении флага архивации
+    EXIT /B
+)
+:LogError <text>
+(
+    ECHO %DATE% %TIME% Ошибка %ERRORLEVEL% %*
+    (ECHO %DATE% %TIME% Ошибка %ERRORLEVEL% %*
+    ) >>"%rarusbackuplogfile%"
+    EXIT /B %ERRORLEVEL%
 )
 :Get1stToken
 (
