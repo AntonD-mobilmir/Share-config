49106db5c8e992b5732a5ee84c29ad286f890976
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-05-08 19:34:25 +0300

    FSACL_PublicDirsRoot.cmd: изменена настройка доступа к общим папкам (в Public), поскольку в некоторых отделах после обновления Windows 10, все Public папки, в том числе рабочий стол, в D:\Users. Теперь доступ для изменения предоставляется только к подпапкам "Documents" "Downloads" "Music" "Pictures" "Recorded TV" "Videos".

--- config/_Scripts/Security/FSACL_PublicDirsRoot.cmd
+++ config/_Scripts/Security/FSACL_PublicDirsRoot.cmd
@@ -18,11 +18,15 @@ SET "UIDAuthenticatedUsers=S-1-5-11;s:y"
 SET "UIDUsers=S-1-5-32-545;s:y"
 SET "UIDSYSTEM=S-1-5-18;s:y"
 SET "UIDCreatorOwner=S-1-3-0;s:y"
+SET "UIDAdministrators=S-1-5-32-544;s:y"
 )
 :Next
 (
-%SetACLexe% -on %1 -ot file -actn clear -clr dacl -actn rstchldrn -rst dacl -actn ace -ace "n:%UIDEveryone%;p:read;i:so,sc" -actn ace -ace "n:%UIDEveryone%;p:traverse,list_dir,read_attr,read_ea,add_file,add_subdir,del_child,read_dacl;i:io,sc" -actn ace -ace "n:%UIDEveryone%;p:write,read,FILE_DELETE_CHILD,DELETE;i:io,so" -ignoreerr -silent
+rem %SetACLexe% -on %1 -ot file -actn clear -clr dacl -actn rstchldrn -rst dacl -actn ace -ace "n:%UIDEveryone%;p:read;i:so,sc" -actn ace -ace "n:%UIDEveryone%;p:traverse,list_dir,read_attr,read_ea,add_file,add_subdir,del_child,read_dacl;i:io,sc" -actn ace -ace "n:%UIDEveryone%;p:write,read,FILE_DELETE_CHILD,DELETE;i:io,so" -ignoreerr -silent
+rem В некоторых отделах после обновления Windows 10, все Public папки, в том числе рабочий стол, в D:\Users
+%SetACLexe% -on %1 -ot file -actn clear -clr dacl -actn rstchldrn -rst dacl -actn ace -ace "n:%UIDEveryone%;p:read;i:so,sc" -actn ace -ace "n:%UIDEveryone%;p:traverse,list_dir,read_attr,read_ea,read_dacl;i:io,sc" -actn ace -ace "n:%UIDAdministrators%;p:full;i:sc,so" -actn ace -ace "n:%UIDSYSTEM%;p:full;i:sc,so" -ignoreerr -silent
 
+FOR %%A IN ("Documents" "Downloads" "Music" "Pictures" "Recorded TV" "Videos") DO %SetACLexe% -on "%~1\%%~A" -ot file -actn ace -actn ace -ace "n:%UIDEveryone%;p:change,FILE_DELETE_CHILD;i:sc" -actn ace -ace "n:%UIDEveryone%;p:write,read,FILE_DELETE_CHILD,DELETE;i:io,so" -ignoreerr -silent
 IF "%~2"=="" EXIT /B
 SHIFT
 GOTO :Next
