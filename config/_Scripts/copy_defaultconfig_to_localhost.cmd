b9de84ae580037d438c39396ea7721c0bed0c874
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-06-23 09:11:16 +0300

    copy_defaultconfig_to_localhost.cmd: если в командной строке параметр не указан, но в _get_defaultconfig_source.cmd указан путь к архиву, будет использоваться существующий DefaultsSource (раньше, если в командной строке ничего не было указано, DefaultsSource заменялся на Apps_roaming)

--- config/_Scripts/copy_defaultconfig_to_localhost.cmd
+++ config/_Scripts/copy_defaultconfig_to_localhost.cmd
@@ -4,18 +4,21 @@ REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 In
 
 IF NOT DEFINED PROGRAMDATA SET "PROGRAMDATA=%ALLUSERSPROFILE%\Application Data"
 )
+SET "DefaultsSourceScript=%ProgramData%\mobilmir.ru\_get_defaultconfig_source.cmd"
+
 IF "%~1"=="" (
-    IF EXIST "%~dp0..\Apps_roaming.7z" (
-	CALL :SplitSrcConfigPath "%~dp0..\Apps_roaming.7z"
-    ) ELSE GOTO :ErrorExit
-) ELSE IF NOT EXIST "%~1" (
-    IF EXIST "%~dp0..\%~1" (
-	CALL :SplitSrcConfigPath "%~dp0..\%~1"
-    ) ELSE GOTO :ErrorExit
-) ELSE CALL :SplitSrcConfigPath %1
+    CALL "%DefaultsSourceScript%"
+    IF DEFINED DefaultsSource (
+	CALL :UsePrefefinedDefaultsSource
+    ) ELSE (
+	IF EXIST "%~dp0..\Apps_roaming.7z" (
+	    CALL :SplitSrcConfigPath "%~dp0..\Apps_roaming.7z"
+	) ELSE GOTO :ErrorExit
+    )
+) ELSE CALL :GetConfigPathWithName "%~1" || GOTO :ErrorExit
 
 rem old location: "%PROGRAMDATA%\mobilmir-config"
-FOR /D %%I IN ("D:\Distributives\config" "W:\Distributives\config" "%PROGRAMDATA%\mobilmir.ru\config") DO (
+FOR /D %%I IN ("D:\Distributives\config" "%PROGRAMDATA%\mobilmir.ru\config") DO (
     IF /I "%srcConfigDir%" EQU "%%~I" (
 	ECHO Конфигурация уже в %%I.
 	SET "configDir=%srcConfigDir%\"
@@ -38,20 +41,38 @@ IF DEFINED configDir (
 
 ECHO Папка настроек: %configDir%
 IF NOT EXIST "%ProgramData%\mobilmir.ru" MKDIR "%ProgramData%\mobilmir.ru"
-SET "DefaultsSourceScript=%ProgramData%\mobilmir.ru\_get_defaultconfig_source.cmd"
 ECHO SET "DefaultsSource=%DefaultsSource%">"%DefaultsSourceScript%"
 
 IF NOT EXIST "%ProgramData%\mobilmir.ru" CALL "%~dp0move Local_Scripts to ProgramData_mobilmir.cmd"
 EXIT /B
 
-:ErrorExit
-    START "Ошибка при выполнении %~nx0" %comspec% /C ECHO Не удалось найти доступный путь для локальной копии папки настроек. Локальная копия не будет создана!^&PAUSE
-    SET "DefaultsSource=%srcConfig%"
+:UsePrefefinedDefaultsSource
+    CALL :GetName configName "%DefaultsSource%"
+(
+    CALL :SplitSrcConfigPath "%configName%"
 EXIT /B
+)
+
+:GetName <var> <path>
+(
+    SET "%~1=%~nx2"
+EXIT /B
+)
+
+:GetConfigPathWithName
+(
+    IF EXIST "%~1" (
+	CALL :SplitSrcConfigPath %1
+    ) ELSE (
+	IF EXIST "%~dp0..\%~1" (
+	    CALL :SplitSrcConfigPath "%~dp0..\%~1"
+	) ELSE EXIT /B 1
+    )
+EXIT /B
+)
 
 :SplitSrcConfigPath
 (
-    SET "srcConfig=%~1"
     SET "srcConfigName=%~nx1"
     SET "srcConfigDir=%~dp1"
 )
@@ -59,3 +80,8 @@ EXIT /B
     SET "srcConfigDir=%srcConfigDir:~0,-1%"
 EXIT /B
 )
+
+:ErrorExit
+    START "Ошибка при выполнении %~nx0" %comspec% /C ECHO Не удалось найти доступный путь для локальной копии папки настроек. Локальная копия не будет создана!^&PAUSE
+    SET "DefaultsSource=%srcConfigDir%%srcConfigName%"
+EXIT /B
