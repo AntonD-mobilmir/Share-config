c542427b19761a93105779e0e83bea3526be4532
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-08-11 11:31:49 +0300

    изменения версий программ, переименованы скрипты software_update, добавлен скрипт foobar2000

--- config_unpacked/_Scripts/software_update_autodist/software_update/scripts/JRE 8u144.cmd
+++ config_unpacked/_Scripts/software_update_autodist/software_update/scripts/JRE 8u144.cmd
@@ -1,18 +1,22 @@
-@REM coding:OEM
-IF NOT DEFINED LOCALAPPDATA SET LOCALAPPDATA=%TEMP%
-SET flagfile=%LOCALAPPDATA%\%~n0-tried-once.flag
+@(REM coding:CP866
+IF NOT DEFINED LOCALAPPDATA SET "LOCALAPPDATA=%TEMP%"
+)
+SET "flagfile=%LOCALAPPDATA%\%~n0-tried-once.flag"
+(
 DEL /Q "%flagfile%"
 
-IF DEFINED log SET JREInstallLog=%log%-msi.log
+IF DEFINED log SET "JREInstallLog=%log%-msi.log"
 
 CALL "%ConfigDir%_Scripts\CheckWinVer.cmd" 6 || EXIT /B
 
+rem Проверка, не установлена ли сейчас последняя версия
 FOR /F "usebackq eol=# tokens=1" %%A IN ("%Distributives%\Soft\System\Virtual Machines Sandboxes\Sun Java\jre8_uids.txt") DO (
     REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{%%A}" /v "DisplayName"
     REM if key not exist, it's not installed, and there will be errorlevel 1. If no error, it's already installed.
     IF NOT ERRORLEVEL 1 EXIT /B
     REM only read first line
     GOTO :endCheck
+)
 )
 :endCheck
 (
