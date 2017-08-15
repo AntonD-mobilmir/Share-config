2bddd12905c343e7d15d330551a728a976c65f37
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-08-15 19:24:07 +0300

    в Mozilla Firefox/install.cmd прописано исключение для Windows Vista

--- Distributives-install-scripts/Soft/Network/HTTP/Mozilla FireFox/install.cmd
+++ Distributives-install-scripts/Soft/Network/HTTP/Mozilla FireFox/install.cmd
@@ -9,14 +9,21 @@ IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
     IF DEFINED ProgramFiles^(x86^) (SET "lProgramFiles=%ProgramFiles(x86)%") ELSE SET "lProgramFiles=%ProgramFiles%"
 
     SET "TempIni=%TEMP%\FirefoxInstall.ini"
-)
-(
-    SET "InstDistributive=%srcpath%Firefox Setup *.exe"
-    SET "MozMainSvcUninst=%lProgramFiles%\Mozilla Maintenance Service\Uninstall.exe"
-    SET "cProgramFiles=%lProgramFiles:\=\\%"
+    FOR /F "usebackq delims=" %%I IN (`ver`) DO SET "winVer=%%~I"
     CALL "%ProgramData%\mobilmir.ru\_get_defaultconfig_source.cmd"
 )
-IF DEFINED DefaultsSource CALL :GetDir ConfigDir "%DefaultsSource%"
+(
+    IF DEFINED DefaultsSource CALL :GetDir ConfigDir "%DefaultsSource%"
+    IF "%winVer:~0,30%"=="Microsoft Windows [Версия 6.0." SET "winVista=1"
+    IF "%winVer:~0,31%"=="Microsoft Windows [Version 6.0." SET "winVista=1"
+    IF DEFINED winVista (
+	IF EXIST "%srcpath%Vista\Firefox Setup *.exe" (
+	    SET "InstDistributive=%srcpath%Vista\Firefox Setup *.exe"
+	) ELSE SET "InstDistributive=%srcpath:\Distributives\Soft\=\Distributives\Soft_old\%Vista\Firefox Setup *.exe"
+    ) ELSE SET "InstDistributive=%srcpath%Firefox Setup *.exe"
+    SET "MozMainSvcUninst=%lProgramFiles%\Mozilla Maintenance Service\Uninstall.exe"
+    SET "cProgramFiles=%lProgramFiles:\=\\%"
+)
 (
     FOR %%A IN ("%InstDistributive%") DO SET "InstDistributive=%%~A"
     COPY /B /Y "%srcpath%install.ini" "%TempIni%"
@@ -44,17 +51,6 @@ EXIT /B
 :HideDesktopShortcut
     REM Hiding desktop shortcut
     SET RegQueryParsingOptions="usebackq tokens=3* delims= "
-    FOR /F "usebackq delims=" %%I IN (`ver`) DO SET "WinVer=%%~I"
-    IF "%WinVer:~0,24%"=="Microsoft Windows 2000 [" GOTO :IncludeRecoding
-    IF "%WinVer:~0,22%"=="Microsoft Windows XP [" GOTO :IncludeRecoding
-    GOTO :SkipRecoding
-:IncludeRecoding
-    rem     there's tab in end of next line. It's mandatory
-    SET RegQueryParsingOptions="usebackq tokens=2* delims=	"
-
-    IF NOT DEFINED recodeexe CALL :findexe recodeexe recode.exe %SystemDrive%\SysUtils\UnxUtils\recode.exe
-    IF DEFINED recodeexe SET "recodecmd=^|%recodeexe% -f --sequence=memory 1251..866"
-:SkipRecoding
 (
     FOR /F %RegQueryParsingOptions% %%I IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Common Desktop" %recodecmd%`) DO SET "CommonDesktop=%%~J"
     IF NOT DEFINED CommonDesktop EXIT /B
