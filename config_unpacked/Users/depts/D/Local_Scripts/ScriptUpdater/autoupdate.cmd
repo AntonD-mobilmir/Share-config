0f1b5677d3df38b588d42b2b786a7f05c42385f9
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-08-18 14:46:28 +0300

    в autoupdate.cmd добавлено обновление MailLoader

--- config_unpacked/Users/depts/D/Local_Scripts/ScriptUpdater/autoupdate.cmd
+++ config_unpacked/Users/depts/D/Local_Scripts/ScriptUpdater/autoupdate.cmd
@@ -18,6 +18,11 @@ IF NOT DEFINED AutohotkeyExe CALL "%configDir%_Scripts\FindAutoHotkeyExe.cmd"
 %AutohotkeyExe% /ErrorStdOut "%ConfigDir%_Scripts\scriptUpdater.ahk" >"%TEMP%\scriptUpdater.ahk.log" 2>&1
 %exe7z% x -y -o"D:\" "%TEMP%\scriptUpdater.ahk.tmp\ScriptUpdater.7z"
 
+IF EXIST D:\1S\Rarus\MailLoader (
+    %AutohotkeyExe% /ErrorStdOut "%ConfigDir%_Scripts\scriptUpdater.ahk" "D:\1S\Rarus\MailLoader\*" "https://www.dropbox.com/s/ylkktduk9ybxjjg/MailLoader-dist.7z.gpg?dl=1" 168 >"%TEMP%\MailLoader-dist.7z.log" 2>&1
+    %exe7z% x -y -o"D:\1S\Rarus\MailLoader\" "%TEMP%\scriptUpdater.ahk.tmp\MailLoader-dist.7z"
+)
+
 SET "GenKeyring="
 FOR %%A IN ("%~dp0gnupg\secring.gpg") DO  (
     IF NOT EXIST "%%~A" SET "GenKeyring=1"
