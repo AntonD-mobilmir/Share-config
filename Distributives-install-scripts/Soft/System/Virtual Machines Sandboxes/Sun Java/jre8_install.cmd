13657c88dcf5b54358493b021fe2b5394a7f3602
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-09-08 12:29:32 +0300

    Sun Java/jre8_install.cmd подготовка для установки 64-битной JRE

--- Distributives-install-scripts/Soft/System/Virtual Machines Sandboxes/Sun Java/jre8_install.cmd
+++ Distributives-install-scripts/Soft/System/Virtual Machines Sandboxes/Sun Java/jre8_install.cmd
@@ -6,9 +6,6 @@ IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
 IF NOT DEFINED PROGRAMDATA SET "PROGRAMDATA=%ALLUSERSPROFILE%\Application Data"
 IF NOT DEFINED APPDATA IF EXIST "%USERPROFILE%\Application Data" SET "APPDATA=%USERPROFILE%\Application Data"
 
-rem Distmask
-SET "distmask=jre-8*-windows-i586.exe"
-
 ECHO. >>"%TEMP%\jre_install.flag"
 
 SET "ProgramFilesx86=%ProgramFiles%"
@@ -17,9 +14,18 @@ IF DEFINED ProgramFiles^(x86^) SET "ProgramFilesx86=%ProgramFiles(x86)%"
 SET JREInstallLogParm=
 IF DEFINED JREInstallLog SET JREInstallLogParm=/L "%JREInstallLog%"
 
-SET reg="%SYSTEMROOT%\System32\REG.EXE"
-REM installing 32-bit JRE. So, if we're on 64-bit windows, must use 32-bit REG.EXE
-IF EXIST "%SYSTEMROOT%\SysWOW64\REG.EXE" SET reg="%SYSTEMROOT%\SysWOW64\REG.EXE"
+IF /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" SET "OS64Bit=1"
+IF DEFINED PROCESSOR_ARCHITEW6432 SET "OS64Bit=1"
+
+SET regexe="%SYSTEMROOT%\System32\REG.EXE"
+
+SET "distmask=jre-8*-windows-i586.exe"
+IF DEFINED OS64Bit IF DEFINED installjre64bit (
+    SET "distmask=jre-8*-windows-x64.exe"
+) ELSE (
+    REM installing 32-bit JRE. So, if we're on 64-bit windows, must use 32-bit REG.EXE
+    IF EXIST "%SYSTEMROOT%\SysWOW64\REG.EXE" SET regexe="%SYSTEMROOT%\SysWOW64\REG.EXE"
+)
 
 CALL :InitRemembering
 )
@@ -35,12 +41,12 @@ rem SET "InstallError=%ERRORLEVEL%" -- EXIT is in same subsection
 REM uninstall previous versions
 IF NOT ERRORLEVEL 1 CALL "%~dp0jre8_uninstall.cmd" /LeaveLast
 
-rem %REG% ADD "HKLM\SOFTWARE\JavaSoft\Java Plug-in\1.6.0_12" /v HideSystemTrayIcon /t REG_DWORD /d 1 /f
-%REG% ADD "HKLM\SOFTWARE\JavaSoft\Java Update\Policy" /v EnableJavaUpdate /t REG_DWORD /d 0 /f
-%REG% ADD "HKLM\SOFTWARE\JavaSoft\Java Update\Policy" /v EnableAutoUpdateCheck /t REG_DWORD /d 0 /f
-%REG% ADD "HKLM\SOFTWARE\JavaSoft\Java Update\Policy" /v NotifyDownload /t REG_DWORD /d 0 /f
-%REG% ADD "HKLM\SOFTWARE\JavaSoft\Java Update\Policy" /v NotifyInstall /t REG_DWORD /d 0 /f
-%REG% DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v SunJavaUpdateSched /f
+rem %regexe% ADD "HKLM\SOFTWARE\JavaSoft\Java Plug-in\1.6.0_12" /v HideSystemTrayIcon /t REG_DWORD /d 1 /f
+%regexe% ADD "HKLM\SOFTWARE\JavaSoft\Java Update\Policy" /v EnableJavaUpdate /t REG_DWORD /d 0 /f
+%regexe% ADD "HKLM\SOFTWARE\JavaSoft\Java Update\Policy" /v EnableAutoUpdateCheck /t REG_DWORD /d 0 /f
+%regexe% ADD "HKLM\SOFTWARE\JavaSoft\Java Update\Policy" /v NotifyDownload /t REG_DWORD /d 0 /f
+%regexe% ADD "HKLM\SOFTWARE\JavaSoft\Java Update\Policy" /v NotifyInstall /t REG_DWORD /d 0 /f
+%regexe% DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v SunJavaUpdateSched /f
 
 REM Uninstall updater
 IF NOT ERRORLEVEL 1 CALL :runmsiexec "%SystemRoot%\System32\msiexec.exe" /x "{4A03706F-666A-4037-7777-5F2748764D10}" /qn
