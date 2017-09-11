e71a77f27305941383a3bddd3e3232f05b09e217
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-09-11 11:59:16 +0300

    ScriptUpdater/autoupdate.cmd: изменен порядок проверки ключа (и генерации при необходимости) и запуска обновлений

--- config_unpacked/Users/depts/D/Local_Scripts/ScriptUpdater/autoupdate.cmd
+++ config_unpacked/Users/depts/D/Local_Scripts/ScriptUpdater/autoupdate.cmd
@@ -15,20 +15,19 @@ rem IF NOT DEFINED SetACLexe CALL "%ConfigDir%_Scripts\find_exe.cmd" SetACLexe S
 IF NOT DEFINED AutohotkeyExe CALL "%configDir%_Scripts\FindAutoHotkeyExe.cmd"
 )
 (
-%AutohotkeyExe% /ErrorStdOut "%ConfigDir%_Scripts\scriptUpdater.ahk" >"%TEMP%\scriptUpdater.ahk.log" 2>&1
-%exe7z% x -y -o"D:\" "%TEMP%\scriptUpdater.ahk.tmp\ScriptUpdater.7z"
-
-IF EXIST D:\1S\Rarus\MailLoader (
-    %AutohotkeyExe% /ErrorStdOut "%ConfigDir%_Scripts\scriptUpdater.ahk" "D:\1S\Rarus\MailLoader\*" "https://www.dropbox.com/s/ylkktduk9ybxjjg/MailLoader-dist.7z.gpg?dl=1" 168 >"%TEMP%\MailLoader-dist.7z.log" 2>&1
-    %exe7z% x -y -o"D:\1S\Rarus\MailLoader\" "%TEMP%\scriptUpdater.ahk.tmp\MailLoader-dist.7z"
-)
-
 SET "GenKeyring="
 FOR %%A IN ("%~dp0gnupg\secring.gpg") DO  (
     IF NOT EXIST "%%~A" SET "GenKeyring=1"
     IF %%~zA. EQU 0. SET "GenKeyring=1"
 )
 IF DEFINED GenKeyring CALL "%~dp0genGpgKeyring.cmd"
+
+%AutohotkeyExe% /ErrorStdOut "%ConfigDir%_Scripts\scriptUpdater.ahk" >"%TEMP%\scriptUpdater.ahk.log" 2>&1
+%exe7z% x -y -o"D:\" "%TEMP%\scriptUpdater.ahk.tmp\ScriptUpdater.7z"
+IF EXIST D:\1S\Rarus\MailLoader (
+    %AutohotkeyExe% /ErrorStdOut "%ConfigDir%_Scripts\scriptUpdater.ahk" "D:\1S\Rarus\MailLoader\*" "https://www.dropbox.com/s/ylkktduk9ybxjjg/MailLoader-dist.7z.gpg?dl=1" 168 >"%TEMP%\MailLoader-dist.7z.log" 2>&1
+    %exe7z% x -y -o"D:\1S\Rarus\MailLoader\" "%TEMP%\scriptUpdater.ahk.tmp\MailLoader-dist.7z"
+)
 
 CALL "%~dp0..\UpdateShortcuts.cmd"
 EXIT /B
