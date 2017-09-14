721da8d652fdda9fbf8bb47adcf6975b9a765277
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-09-14 18:14:13 +0300

    D_1S_Rarus_ShopBTS_ShopBTS_Add/register_all_components.cmd будет пропускать регистрацию Shop2EL.dll при первом проходе (иначе regsvr32 повисает)

--- Rarus_ShopBTS_InitialBase/D_1S_Rarus_ShopBTS_ShopBTS_Add/register_all_components.cmd
+++ Rarus_ShopBTS_InitialBase/D_1S_Rarus_ShopBTS_ShopBTS_Add/register_all_components.cmd
@@ -1,5 +1,5 @@
 @(REM coding:CP866
-FOR %%A IN ("%~dp0*.dll" "%~dp0*.ocx") DO %SystemRoot%\System32\regsvr32.exe /s "%%~A" || CALL :EchoError "%%~A"
+FOR %%A IN ("%~dp0*.dll" "%~dp0*.ocx") DO IF /I "%%~nxA" NEQ "Shop2EL.dll" %SystemRoot%\System32\regsvr32.exe /s "%%~A" || CALL :EchoError "%%~A"
 
 %SystemRoot%\System32\ping.exe -n 5 127.0.0.1 >NUL
 %SystemRoot%\System32\regsvr32.exe /s "%~dp0Shop2EL.dll"
