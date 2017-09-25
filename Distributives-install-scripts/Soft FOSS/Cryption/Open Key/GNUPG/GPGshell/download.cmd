8fa6ecd9051c62c0714730971163c32f425a52c3
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-09-25 11:41:28 +0300

    удалены устаревшие утилиты из Distributives\…\GNUPG\*

--- Distributives-install-scripts/Soft FOSS/Cryption/Open Key/GNUPG/GPGshell/download.cmd
+++
@@ -1,5 +0,0 @@
-@REM coding:OEM
-SET srcpath=%~dp0
-SET distcleanup=1
-IF NOT DEFINED baseScripts SET baseScripts=\Scripts
-CALL "%baseScripts%\_DistDownload.cmd" http://www.jumaros.de/rsoft/download/gpgsh.php *.zip
