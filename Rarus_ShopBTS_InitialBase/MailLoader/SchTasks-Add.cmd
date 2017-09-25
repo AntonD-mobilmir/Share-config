4932abec28ed7b25c49eddc981a5e3bd6bdfdcb5
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-09-25 19:20:26 +0300

    MailLoader/SchTasks-Add.cmd будет повторно пытаться добавить задачу при ошибке

--- Rarus_ShopBTS_InitialBase/MailLoader/SchTasks-Add.cmd
+++ Rarus_ShopBTS_InitialBase/MailLoader/SchTasks-Add.cmd
@@ -4,9 +4,10 @@ REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 In
 
 IF NOT DEFINED System32 IF EXIST "%SystemRoot%\SysNative\cmd.exe" (SET "System32=%SystemRoot%\SysNative") ELSE SET "System32=%SystemRoot%\System32"
 )
+:again
 (
 rem %System32%\SCHTASKS.exe /Delete /TN "mobilmir\%~n1" /F
-%System32%\SCHTASKS.exe /Create /F /TN "mobilmir.ru\%~n1" /XML %* || EXIT /B
+%System32%\SCHTASKS.exe /Create /F /TN "mobilmir.ru\%~n1" /XML %* || GOTO :again
 %System32%\SCHTASKS.exe /Run /TN "mobilmir.ru\%~n1"
 EXIT /B
 )
