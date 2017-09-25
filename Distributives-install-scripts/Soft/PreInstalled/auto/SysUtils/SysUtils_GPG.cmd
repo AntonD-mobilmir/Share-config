0dbe57ab7b386a90733bcd4eb4bf8fcedb5f5d54
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-09-25 11:47:09 +0300

    gpg4win обновлен до 3.0, теперь утилиты создания контрольных сумм не входят в комплект gpg, также gnupg\pub\gpg.exe → gnupg\gpg.exe

--- Distributives-install-scripts/Soft/PreInstalled/auto/SysUtils/SysUtils_GPG.cmd
+++ Distributives-install-scripts/Soft/PreInstalled/auto/SysUtils/SysUtils_GPG.cmd
@@ -2,7 +2,7 @@
 IF NOT DEFINED SysUtilsDir CALL "%~dp0_init.cmd"
 )
 (
-IF NOT DEFINED pathString SET "pathString=%SysUtilsDir%\gnupg\pub"
+IF NOT DEFINED pathString SET "pathString=%SysUtilsDir%\gnupg"
 "%utilsdir%7za.exe" x -r -aoa -o"%SysUtilsDir%" "%~dpn0.7z"
 IF NOT "%SysUtilsDelaySettings%"=="1" CALL "%~dp0_finalize.cmd"
 )
