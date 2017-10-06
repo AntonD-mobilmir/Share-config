79e536aba3e1523f72cad3d967e9b252e6df19d3
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-10-06 09:57:54 +0300

    чтобы избежать смены даты папки TrID при обновлении, triddefs.dat (и, соответственно, изменения архива с плагинами), выкачивание архивов выполняется во временную папку, и заменяются они только после сравнения

--- Distributives_unpacked/Soft/PreInstalled/manual/TotalCommander.config/PlugIns/wdx/TrID_Identifier/TrID/update.cmd
+++ Distributives_unpacked/Soft/PreInstalled/manual/TotalCommander.config/PlugIns/wdx/TrID_Identifier/TrID/update.cmd
@@ -1,4 +1,28 @@
-@REM coding:OEM
-wget -N http://goo.gl/aJVb http://goo.gl/Bnw1
-..\..\..\wcx\Total7zip\7z.exe x -aoa trid_w32.zip
-..\..\..\wcx\Total7zip\7z.exe x -aoa triddefs.zip
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
+SETLOCAL ENABLEEXTENSIONS
+
+MKDIR "%TEMP%\%~nx0.tmp"
+PUSHD "%TEMP%\%~nx0.tmp" && (
+    wget -N http://goo.gl/aJVb http://goo.gl/Bnw1
+    CALL :UpdateFile trid_w32.zip *
+    CALL :UpdateFile triddefs.zip triddefs.trd
+    POPD
+)
+RD /S /Q "%TEMP%\%~nx0.tmp"
+EXIT /B
+)
+:UpdateFile <arcPath> <mask>
+(
+"%~dp0..\..\..\wcx\Total7zip\7zg.exe" x -aoa -o"%TEMP%\%~nx0.tmp\%~nx1.tmp" -- %*
+PUSHD "%TEMP%\%~nx0.tmp\%~nx1.tmp" && (
+    FOR %%A IN (%2) DO (
+	FC /B /LB1 /A "%%~A" "%~dp0%%~A" >NUL
+	IF ERRORLEVEL 2 PAUSE
+	IF ERRORLEVEL 1 MOVE /Y "%%~A" "%~dp0%%~A"
+    )
+    POPD
+)
+EXIT /B
+)
