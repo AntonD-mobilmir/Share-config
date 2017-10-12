e4e38be4a9ab5f7b5ef57867d1bd44dfc6dbbfc6
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-10-12 18:52:26 +0300

    autoupdate.cmd: отключен запуск генерации ключа при его отсутствии; исправлены мелкие ошибки

--- config_unpacked/_Scripts/ScriptUpdater_dist/ScriptUpdater/autoupdate.cmd
+++ config_unpacked/_Scripts/ScriptUpdater_dist/ScriptUpdater/autoupdate.cmd
@@ -15,21 +15,23 @@ rem IF NOT DEFINED SetACLexe CALL "%configDir%_Scripts\find_exe.cmd" SetACLexe S
 IF NOT DEFINED AutohotkeyExe CALL "%configDir%_Scripts\FindAutoHotkeyExe.cmd"
 )
 (
-SET "GenKeyring="
-FOR %%A IN ("%~dp0gnupg\secring.gpg") DO  (
-    IF NOT EXIST "%%~A" SET "GenKeyring=1"
-    IF %%~zA. EQU 0. SET "GenKeyring=1"
-)
-IF DEFINED GenKeyring CALL "%~dp0genGpgKeyring.cmd"
+rem SET "GenKeyring="
+rem FOR %%A IN ("%~dp0gnupg\secring.gpg") DO  (
+rem     IF NOT EXIST "%%~A" SET "GenKeyring=1"
+rem     IF %%~zA. EQU 0. SET "GenKeyring=1"
+rem )
+rem IF DEFINED GenKeyring CALL "%~dp0genGpgKeyring.cmd"
 
 %AutohotkeyExe% /ErrorStdOut "%~dp0scriptUpdater.ahk" "%~dp0*" "https://www.dropbox.com/s/y6xpm8xgcovkffg/ScriptUpdater.7z.gpg?dl=1" 48 >"%TEMP%\scriptUpdater.ahk.log" 2>&1 && %exe7z% x -y -o"D:\" "%TEMP%\scriptUpdater.ahk.tmp\ScriptUpdater.7z"
 IF EXIST D:\1S\Rarus\MailLoader (
     %AutohotkeyExe% /ErrorStdOut "%~dp0scriptUpdater.ahk" "D:\1S\Rarus\MailLoader\*" "https://www.dropbox.com/s/ylkktduk9ybxjjg/MailLoader-dist.7z.gpg?dl=1" 168 >"%TEMP%\MailLoader-dist.7z.log" 2>&1
     %exe7z% x -y -o"D:\1S\Rarus\MailLoader\" "%TEMP%\scriptUpdater.ahk.tmp\MailLoader-dist.7z"
 )
-IF EXIST D:\Local_Scripts %AutohotkeyExe% /ErrorStdOut "%~dp0scriptUpdater.ahk" D:\Local_Scripts\*
+IF EXIST D:\Local_Scripts (
+    %AutohotkeyExe% /ErrorStdOut "%~dp0scriptUpdater.ahk" D:\Local_Scripts\*
+    CALL "D:\Local_Scripts\UpdateShortcuts.cmd"
+)
 
-CALL "%~dp0..\UpdateShortcuts.cmd"
 EXIT /B
 )
 :GetDir
