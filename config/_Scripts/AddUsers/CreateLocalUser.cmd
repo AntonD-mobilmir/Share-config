9c405383818c9c8a0a9a7637a5f83b8cd5934ce7
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-10-23 09:29:05 +0300

    CreateLocalUser.cmd: скрипт для создания локального пользователя, выключения требования смены пароля при первом входе и добавления в группу пользователей удалённого рабочего стола

---
+++ config/_Scripts/AddUsers/CreateLocalUser.cmd
@@ -0,0 +1,29 @@
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
+ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
+IF NOT DEFINED PROGRAMDATA SET "PROGRAMDATA=%ALLUSERSPROFILE%\Application Data"
+IF NOT DEFINED APPDATA IF EXIST "%USERPROFILE%\Application Data" SET "APPDATA=%USERPROFILE%\Application Data"
+
+    CALL :SetOrAsk newUserName "Имя пользователя (логин)" "%~1"
+    CALL :SetOrAsk FullName "Полное имя (ФИО по-русски)" "%~2"
+    CALL :SetOrAsk Note "Описание (см. trello.com/c/uJ4B9C7w)" "%~3"
+)
+(
+    %SystemRoot%\System32\net.exe USER "%newUserName%" * /Add /FULLNAME:"%FullName%" /USERCOMMENT:"%Note%" || EXIT /B
+    %SystemRoot%\System32\net.exe USER "%newUserName%" /LOGONPASSWORDCHG:NO
+    %SystemRoot%\System32\net.exe LOCALGROUP "Пользователи удаленного рабочего стола" "%USER%" /Add
+EXIT /B
+)
+
+:SetOrAsk <varName> <description> <value>
+(
+    IF NOT "%~3"=="" (
+	SET "%~1=%~3"
+    ) ELSE IF NOT "%RunInteractiveInstalls%"=="0" (
+	SET /P "%~1=%~2: "
+    ) ELSE EXIT /B 1
+EXIT /B
+)
