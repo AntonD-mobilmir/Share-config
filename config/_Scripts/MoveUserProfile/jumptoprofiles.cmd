dd73174308388d884e6f88bbda3445836154623e
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-11-09 11:06:54 +0300

    jumptoprofiles.cmd: вместо regjump, прописывает путь к ветке в настройки regedit

--- config/_Scripts/MoveUserProfile/jumptoprofiles.cmd
+++ config/_Scripts/MoveUserProfile/jumptoprofiles.cmd
@@ -1,2 +1,7 @@
-@REM coding:OEM
-regjump HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
+
+%SystemRoot%\System32\REG.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit" /t REG_SZ /v "LastKey" /d "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" /f
+START "" /B %SystemRoot%\regedit.exe
+)
