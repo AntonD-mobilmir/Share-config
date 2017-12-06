dde60598200f3de2bcb04b9913a4a3b081ad23f3
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2017-12-06 18:28:51 +0300

    настройка компонентов Windows оптимизирована (одной командой добавляется/удаляется сразу набор компонентов ), добавлен скрипт для Windows 10

---
+++ config/_Scripts/Windows Components/SetupComponents10.cmd
@@ -0,0 +1,19 @@
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
+SETLOCAL ENABLEEXTENSIONS
+
+IF DEFINED PROCESSOR_ARCHITEW6432 (
+    "%SystemRoot%\SysNative\cmd.exe" /C %0 %*
+    EXIT /B
+)
+
+
+"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Windows-Defender-Default-Definitions" /FeatureName:"FaxServicesClientPackage"
+
+rem +useful components
+"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"TelnetClient" /FeatureName:"TFTP"
+rem "%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"TelnetServer" /FeatureName:"ScanManagementConsole" /FeatureName:"Printing-XPSServices-Features" /FeatureName:"TIFFIFilter"
+
+EXIT /B
+)
--- config/_Scripts/Windows Components/SetupComponents8.cmd
+++ config/_Scripts/Windows Components/SetupComponents8.cmd
@@ -1,34 +1,15 @@
-@REM coding:OEM
+@(REM coding:OEM
 IF DEFINED PROCESSOR_ARCHITEW6432 (
     "%SystemRoot%\SysNative\cmd.exe" /C %0 %*
     EXIT /B
 )
 
-rem Windows 7 Pro
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"MediaCenter"
-
 rem -games
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Solitaire"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"SpiderSolitaire"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Hearts"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"FreeCell"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Minesweeper"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"PurblePlace"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Chess"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Shanghai"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Internet Checkers"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Internet Backgammon"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Internet Spades"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Internet Games"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"InboxGames"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"More Games"
+"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Disable-Feature /FeatureName:"Solitaire" /FeatureName:"SpiderSolitaire" /FeatureName:"Hearts" /FeatureName:"FreeCell" /FeatureName:"Minesweeper" /FeatureName:"PurblePlace" /FeatureName:"Chess" /FeatureName:"Shanghai" /FeatureName:"Internet Checkers" /FeatureName:"Internet Backgammon" /FeatureName:"Internet Spades" /FeatureName:"Internet Games" /FeatureName:"InboxGames" /FeatureName:"More Games"
 
 rem +useful components
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"TelnetClient"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"TelnetServer"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"ScanManagementConsole"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"TFTP"
-"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"Printing-XPSServices-Features"
-REM "%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"TIFFIFilter"
+"%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"TelnetClient" /FeatureName:"TFTP"
+rem "%SystemRoot%\System32\Dism.exe" /Online /NoRestart /Enable-Feature /FeatureName:"TelnetServer" /FeatureName:"ScanManagementConsole" /FeatureName:"Printing-XPSServices-Features" /FeatureName:"TIFFIFilter"
 
 EXIT /B
+)
