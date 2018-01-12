eb5eff3c046b7cd95bc48335ce51ff6a802100d1
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2018-01-12 13:22:22 +0300

    На 64-битных Windows будет устанавливаться только JRE9

--- Distributives-install-scripts/Soft/System/Virtual Machines Sandboxes/Sun Java/jre_install.cmd
+++ Distributives-install-scripts/Soft/System/Virtual Machines Sandboxes/Sun Java/jre_install.cmd
@@ -1,4 +1,13 @@
-@REM coding:OEM
-IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
 
-CALL "%~dp0jre8_install.cmd"
+    IF /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" SET "OS64Bit=1"
+    IF DEFINED PROCESSOR_ARCHITEW6432 SET "OS64Bit=1"
+
+    IF DEFINED OS64Bit (
+	CALL "%~dp0jre9_install.cmd"
+    ) ELSE (
+	CALL "%~dp0jre8_install.cmd"
+    )
+)
