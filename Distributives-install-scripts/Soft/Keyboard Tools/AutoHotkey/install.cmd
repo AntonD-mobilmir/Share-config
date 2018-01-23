9e9af630ebf0e65bf2bf8e96f4c0b7ce220a63d6
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2018-01-23 11:28:59 +0300

    AutoHotkey/install.cmd теперь переименовывает autohotkey.exe перед установкой, чтобы избежать блокировки, если запущены какие-то скрипты

--- Distributives-install-scripts/Soft/Keyboard Tools/AutoHotkey/install.cmd
+++ Distributives-install-scripts/Soft/Keyboard Tools/AutoHotkey/install.cmd
@@ -1,61 +1,58 @@
 @(REM coding:CP866
 REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
-IF DEFINED PROCESSOR_ARCHITEW6432 IF NOT DEFINED AutoHotkeyInstallRestart (
-    SET "AutoHotkeyInstallRestart=1"
-    "%SystemRoot%\SysNative\cmd.exe" /C %0 %*
-    EXIT /B
-)
-SETLOCAL ENABLEEXTENSIONS
-IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
-IF NOT DEFINED PROGRAMDATA SET "PROGRAMDATA=%ALLUSERSPROFILE%\Application Data"
-IF NOT DEFINED APPDATA IF EXIST "%USERPROFILE%\Application Data" SET "APPDATA=%USERPROFILE%\Application Data"
-)
-FOR /F "usebackq delims=" %%I IN (`DIR /B /O-D "%srcpath%AutoHotkey_*_setup.exe"`) DO (
-    "%srcpath%%%~I" /s
-    rem to extract without associating: /D="%ProgramFiles%\AutoHotkey" /E
-    GOTO :ExitForFindInstaller
-)
-:ExitForFindInstaller
-(
-FOR /F "usebackq tokens=2 delims==" %%I IN (`FTYPE AutoHotkeyScript`) DO CALL :GetFirstArg AutohotkeyExe %%I
+    IF DEFINED PROCESSOR_ARCHITEW6432 IF NOT DEFINED AutoHotkeyInstallRestart (
+	SET "AutoHotkeyInstallRestart=1"
+	"%SystemRoot%\SysNative\cmd.exe" /C %0 %*
+	EXIT /B
+    )
+    SETLOCAL ENABLEEXTENSIONS
+    IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
+    IF NOT DEFINED PROGRAMDATA SET "PROGRAMDATA=%ALLUSERSPROFILE%\Application Data"
+    IF NOT DEFINED APPDATA IF EXIST "%USERPROFILE%\Application Data" SET "APPDATA=%USERPROFILE%\Application Data"
+
+    CALL :FindAutohotkeyExe
 )
 (
-CALL :XPFixACL %AutohotkeyExe%
+    IF DEFINED AutohotkeyExe FOR %%A IN (%AutohotkeyExe%) DO CALL :UnlockByMoving "%%~A" "%%~tA %RANDOM%.bak"
+    FOR /F "usebackq delims=" %%I IN (`DIR /B /O-D "%srcpath%AutoHotkey_*_setup.exe"`) DO (
+	"%srcpath%%%~I" /s
+	rem to extract without associating: /D="%ProgramFiles%\AutoHotkey" /E
+	CALL :FindAutohotkeyExe && GOTO :InstalledSuccessfully
+    )
+)
+:InstalledSuccessfully
+(
 REM Hiding shortcuts
 %AutohotkeyExe% /ErrorStdOut "%~dp0HideStartMenuShortcuts.ahk"
+REM unpacking Lib
 IF NOT DEFINED AutoHotkey_Lib_reentrance (
     SET /A "AutoHotkey_Lib_reentrance=1"
     CALL "%~dp0..\..\PreInstalled\auto\AutoHotkey_Lib.cmd"
 )
-FOR %%I IN (%AutohotkeyExe%) DO IF NOT EXIST "%%~dpILib\*" CALL :CheckAndLink "%ProgramFiles(x86)%\AutoHotkey\Lib" "%%~dpILib" || CALL :CheckAndLink "%ProgramFiles%\AutoHotkey\Lib" "%%~dpILib"
 EXIT /B
 )
-
-:CheckAndLink
+:FindAutohotkeyExe
 (
-    IF EXIST "%~1" (
-	"%SystemDrive%\SysUtils\xln.exe" -n %1 %2
-    ) ELSE EXIT /B 1
-    EXIT /B
+FOR /F "usebackq tokens=2 delims==" %%I IN (`FTYPE AutoHotkeyScript`) DO CALL :GetFirstArg AutohotkeyExe %%I
+EXIT /B
 )
 :GetFirstArg
 (
     SET %1=%2
 EXIT /B
 )
-:XPFixACL
+:UnlockByMoving <path> <suffix>
 (
-    REM On Windows XP, permissions are broken after install for some reson: only admins have AutoHotkey.exe access
-    FOR /F "usebackq delims=" %%W IN (`ver`) DO SET "VW=%%W"
+    IF NOT EXIST %1 EXIT /B 1
+    SET "suffix=%~2"
 )
+SET suffix=%suffix::=%
 (
-    IF NOT "%VW:~0,22%"=="Microsoft Windows XP [" EXIT /B
-    %SetACLexe% -on "%~dp1" -ot file -rec cont_obj -actn rstchldrn -rst dacl -actn setowner -ownr "S-1-5-32-544;s:y" -ignoreerr -silent
-    IF ERRORLEVEL 9009 IF NOT ERRORLEVEL 9010 (
-	REM there is no icacls.exe cacls on XP
-	%SystemRoot%\System32\cacls.exe %1 /e /g Everyone:r
-	%SystemRoot%\System32\cacls.exe %1 /e /g Все:r
-    )
-EXIT /B
+    rem create non-locked copy for the case AutohotkeyExe is currently running
+    MOVE /Y %1 "%~1.%suffix%" && COPY /B "%~1.%suffix%" %1 || EXIT /B
+    rem try to remove it right away in case it wasn't really locked
+    DEL "%~1.%suffix%"
+    rem exit without error even if file can't be deleted, since there were no unlock errors [checked previously]
+EXIT /B 0
 )
