bd1c75140f67280b09a705166b23bc1201fefcb4
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2018-06-01 16:56:50 +0300

    [+] Inventory/old/repack_all.cmd – перепакует отчеты WinAudit из отдельных *.7z в один большой архив

---
+++ Inventory/old/repack_all.cmd
@@ -0,0 +1,12 @@
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/>.
+SETLOCAL ENABLEEXTENSIONS
+
+SET "today=%DATE:~-4,4%-%DATE:~-7,2%-%DATE:~-10,2%"
+CALL "%~dp0..\..\config\_Scripts\find7zexe.cmd"
+)
+(
+FOR /R "%~dp0" %%A IN (*.7z) DO %exe7z% x -aoa -o"%%~dpA*" -- "%%~A" && DEL "%%~A"
+START "" /B /WAIT /D "%~dp0" %exe7z% a -sdel -mx=9 -m0=LZMA2:a=2:fb=273 -mqs=on -x!"%today%.7z" -x!"%~nx0" -x!"board-dumps" -- "%today%.7z"
+)
