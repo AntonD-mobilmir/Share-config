c663de4216c0cc4722ed737bc496d5eb44c5aff0
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2018-08-21 13:06:28 +0300

    Disable SuperFetch (for SSD).cmd удалён

--- config/_Scripts/registry/Disable SuperFetch (for SSD).cmd
+++
@@ -1,8 +0,0 @@
-@(REM coding:CP866
-REM by LogicDaemon <www.logicdaemon.ru>
-REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
-SETLOCAL ENABLEEXTENSIONS
-
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v "EnablePrefetcher" /t REG_DWORD /d 0 /f /reg:64
-RD /s /q "%windir%\Prefetch"
-)
