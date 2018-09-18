9eea6a293349fea7220b3c578a87681e556593a0
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2018-09-18 13:33:37 +0300

    FreeVImager теперь Donationware, а не FOSS. Последняя FOSS-версия 7.7.0, она и останется в дистрибутивах. 9.0.0 будет автообновляться в соответствующей папке.

--- Distributives-install-scripts/Soft FOSS/Graphics/Viewers Managers/FreeVimager/.Distributives_Update_Run.none.cmd
+++ Distributives-install-scripts/Soft FOSS/Graphics/Viewers Managers/FreeVimager/.Distributives_Update_Run.none.cmd
@@ -1,3 +1,4 @@
+rem FreeVImager is now Donationware. No update!
 @(REM coding:OEM
 SETLOCAL ENABLEEXTENSIONS
 SET "srcpath=%~dp0"
@@ -12,10 +13,11 @@ rem relpath is srcpath relatively to baseDistributives (no trailing backslash)
 rem workdir - baseWorkdir with relpath (or %srcpath%temp if baseWorkdir isn't defined)
 rem logsDir - baseLogsDir with relpath (or nothing)
 IF NOT DEFINED logsDir SET "logsDir=%workdir%"
+
 )
 (
-CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/russian/ FreeVimager-*-Portable-Rus.exe -ml1 -nd -A.exe
+rem CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/russian/ FreeVimager-*-Portable-Rus.exe -ml1 -nd -A.exe
 rem to Soft\ [other dir] CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/russian/ FreeVimager-*-Setup-Rus.exe -ml1 -nd -A.exe
-CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/english/ FreeVimager-*-Portable.exe -ml1 -nd -A.exe
-CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/english/ FreeVimager-*-Setup.exe -ml1 -nd -A.exe
+rem CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/english/ FreeVimager-*-Portable.exe -ml1 -nd -A.exe
+rem CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/english/ FreeVimager-*-Setup.exe -ml1 -nd -A.exe
 )
