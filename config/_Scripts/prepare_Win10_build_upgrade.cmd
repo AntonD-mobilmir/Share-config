8fc6b5d5a0a4f30ab1a30c9054e4338117c34dc4
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2018-10-04 10:30:45 +0300

    prepare_Win10_build_upgrade.cmd: подготовка Windows к обновлению сборки – удаление ПО, возврат папки профилей и общих папок на системный диск

---
+++ config/_Scripts/prepare_Win10_build_upgrade.cmd
@@ -0,0 +1,16 @@
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/>.
+SETLOCAL ENABLEEXTENSIONS
+
+CALL "%~dp0cleanup\uninstall_soft.cmd"
+CALL "%~dp0cleanup\cleanmgr-full.cmd"
+START "BleachBit" /WAIT /MIN %comspec% /C "%~dp0cleanup\BleachBit-auto.cmd" /ProfileOnSystemDrive
+RD /S /Q "%APPDATA%\..\LocalLow\Sun"
+REM Удаление БД Offline-файлов https://support.microsoft.com/en-us/help/942974/
+%SystemRoot%\System32\REG.exe ADD "HKLM\System\CurrentControlSet\Services\CSC\Parameters" /v FormatDatabase /t REG_DWORD /d 1 /f
+
+"%~dp0FindAutoHotkeyExe.cmd" "%~dp0MoveUserProfile\Restore Profiles Directory from backup.ahk"
+REG IMPORT "%~dp0MoveUserProfile\HKLM ProfilesDirectory SystemDrive_Users.reg"
+REG IMPORT "%~dp0MoveUserProfile\HKLM User Shell Folders original.reg"
+)
