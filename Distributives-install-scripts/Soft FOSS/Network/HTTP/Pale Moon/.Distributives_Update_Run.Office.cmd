3fa1f34019c78cb3edcd14e7ed0e44c29e87e815
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2018-11-20 15:37:51 +0300

    [*] исправлен/переписан скрипт загрузки дистрибутива Pale Moon

--- Distributives-install-scripts/Soft FOSS/Network/HTTP/Pale Moon/.Distributives_Update_Run.Office.cmd
+++ Distributives-install-scripts/Soft FOSS/Network/HTTP/Pale Moon/.Distributives_Update_Run.Office.cmd
@@ -1,2 +1,7 @@
 @REM coding:OEM
-CALL "%~dp0download.cmd" %*
+SET srcpath=%~dp0
+SET distcleanup=1
+IF NOT DEFINED baseScripts SET baseScripts=\Scripts
+rem CALL "%baseScripts%\_DistDownload.cmd" http://www.palemoon.org/download-ng.shtml *.exe -m -l 1 -HD mirror.palemoon.org -A .exe -nd
+CALL "%baseScripts%\_DistDownload.cmd" "https://www.palemoon.org/download.php?mirror=eu&bits=32&type=installer" palemoon-*.win32.installer.exe -m -l 1 -H -A .exe -nd
+CALL "%baseScripts%\_DistDownload.cmd" "https://www.palemoon.org/download.php?mirror=eu&bits=64&type=installer" palemoon-*.win64.installer.exe -m -l 1 -H -A .exe -nd
--- Distributives-install-scripts/Soft FOSS/Network/HTTP/Pale Moon/download.cmd
+++
@@ -1,5 +0,0 @@
-@REM coding:OEM
-SET srcpath=%~dp0
-SET distcleanup=1
-IF NOT DEFINED baseScripts SET baseScripts=\Scripts
-CALL "%baseScripts%\_DistDownload.cmd" http://www.palemoon.org/download-ng.shtml *.exe -m -l 1 -HD mirror.palemoon.org -A .exe -nd
