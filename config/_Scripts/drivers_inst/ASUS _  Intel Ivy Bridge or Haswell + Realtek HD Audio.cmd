ff6074040b20d07fec4471088056fffa22c930a0
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-01-24 13:13:55 +0300

    [-] config/_Scripts/drivers_inst: с Windows 10 скрипты потеряли актуальность

--- config/_Scripts/drivers_inst/ASUS _  Intel Ivy Bridge or Haswell + Realtek HD Audio.cmd
+++
@@ -1,5 +0,0 @@
-@REM coding:OEM
-SETLOCAL ENABLEEXTENSIONS
-
-START "" /B /WAIT %comspec% /C "%~dp0ASUS PNP0A0A.cmd"
-CALL "%~dp0Intel Ivy Bridge or Haswell + Realtek HD Audio.cmd"
--- config/_Scripts/drivers_inst/Intel Haswell + Realtek HD Audio.cmd
+++
@@ -1,8 +0,0 @@
-@REM coding:OEM
-SETLOCAL ENABLEEXTENSIONS
-
-START "" /B /WAIT %comspec% /C "\\Srv0.office0.mobilmir\Distributives\Drivers\Intel\Chipset\Chipset Software Installation Utility\install.cmd"
-START "" /B /WAIT %comspec% /C "\\Srv0.office0.mobilmir\Distributives\Drivers\Intel\Chipset\Intel Management Engine Interface\WU\Install.cmd"
-START "" /B /WAIT %comspec% /C "\\Srv0.office0.mobilmir\Distributives\Drivers\Intel\Graphics\4th gen - Haswell\install.cmd"
-START "" /B /WAIT %comspec% /C "\\Srv0.office0.mobilmir\Distributives\Drivers\Intel\Chipset\USB 3.0 XHCI\install_8series.cmd"
-START "" /B /WAIT %comspec% /C "\\Srv0.office0.mobilmir\Distributives\Drivers\Realtek\Audio\High_Definition_Audio_Codecs\Install.cmd"
