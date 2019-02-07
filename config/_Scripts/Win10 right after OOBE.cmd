712e977e38a51a6df3aad1990c0a007fd69f85dc
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-02-07 16:13:27 +0300

    Win10 right after OOBE.cmd: запись журналов; ожидание завершения скриптов; выполнение Remove All AppX Apps for current user.cmd после Remove AppX Apps except allowed.cmd, а не одновременно

--- config/_Scripts/Win10 right after OOBE.cmd
+++ config/_Scripts/Win10 right after OOBE.cmd
@@ -3,17 +3,32 @@ REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
 SETLOCAL ENABLEEXTENSIONS
 
-CALL :bg "%~dp0Security\AppLocker - Deny promoted apps Win10.cmd"
-CALL :bg "%~dp0cleanup\AppX\Remove AppX Apps except allowed.cmd" /quiet
-CALL :bg "%~dp0cleanup\AppX\Remove All AppX Apps for current user.cmd" /firstlogon
-CALL :bg "%~dp0registry\DefaultUserRegistrySettings.cmd"
+    MKDIR "%TEMP%\%~n0.tmp"
+    SET "tmp=%TEMP%\%~n0.tmp"
 
-CALL :bg "%~dp0FindAutoHotkeyExe.cmd" "%~dp0cleanup\uninstall\050 OneDrive.ahk"
+    CALL :bg "%~dp0Security\AppLocker - Deny promoted apps Win10.cmd"
+    CALL :bg "%~dp0registry\DefaultUserRegistrySettings.cmd"
+
+    CALL :bg "%~dp0FindAutoHotkeyExe.cmd" "%~dp0cleanup\uninstall\050 OneDrive.ahk"
+
+    powershell.exe -command "Get-AppXPackage | Format-Table -GroupBy PublisherId | Out-String -Width 1048576 | Out-File "%TEMP%\all-AppXPackages.txt""
+
+    CALL :bg "%~dp0cleanup\AppX\Remove AppX Apps except allowed.cmd" /quiet
+)
+:again
+@(
+    IF EXIST "%tmp%\*.tmp" (
+        PING -n 3 127.0.0.1 >NUL
+        FOR %%A IN ("%tmp%\*.tmp") DO MOVE /Y "%%~A" "%%~dpnA.log" >NUL 2>NUL
+        GOTO :again
+    )
+    
+    CALL :bg "%~dp0cleanup\AppX\Remove All AppX Apps for current user.cmd" /firstlogon
 EXIT /B
 )
 
 :bg
 (
-    START "Running %~nx1" /LOW %comspec% /C "%*"
+    START "Running %~nx1" /LOW %comspec% /C "%* >"%tmp%\%~nx1.tmp" 2>&1"
 EXIT /B
 )
