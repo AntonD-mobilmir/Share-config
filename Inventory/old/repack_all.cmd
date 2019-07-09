f2344171c24fea671d9ebe8bd641a8c443367772
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-07-09 18:58:19 +0300

    Inventory/old/repack_all.cmd: добавлен пропуск предыдущих архивов

--- Inventory/old/repack_all.cmd
+++ Inventory/old/repack_all.cmd
@@ -5,8 +5,15 @@ SETLOCAL ENABLEEXTENSIONS
 
 SET "today=%DATE:~-4,4%-%DATE:~-7,2%-%DATE:~-10,2%"
 CALL "%~dp0..\..\config\_Scripts\find7zexe.cmd"
+FOR /D %%D IN ("%~dp0*.*") DO CALL :unpackAllArchives "%%~D"
 )
 (
-FOR /R "%~dp0" %%A IN (*.7z) DO %exe7z% x -aoa -o"%%~dpA*" -- "%%~A" && DEL "%%~A"
-START "" /B /WAIT /D "%~dp0" %exe7z% a -sdel -mx=9 -m0=LZMA2:a=2:fb=273 -mqs=on -x!"%today%.7z" -x!"%~nx0" -x!"board-dumps" -- "%today%.7z"
+START "" /B /WAIT /D "%~dp0" %exe7z% a -sdel -mx=9 -m0=LZMA2:a=2:fb=273 -mqs=on -x!"*.7z" -x!"%~nx0" -x!"board-dumps" -- "%today%.7z"
+EXIT /B
+)
+
+:unpackAllArchives
+(
+    FOR /R "%~1" %%A IN (*.7z) DO %exe7z% x -aoa -o"%%~dpA*" -- "%%~A" && DEL "%%~A"
+EXIT /B
 )
