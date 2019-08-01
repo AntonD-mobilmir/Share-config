9cc3dfca448685506ecdf706f8f1648bb3b3bebf
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-08-01 18:56:49 +0300

    CreateSharedAccount_user_Hostname.ahk – создает пользователя "user %Hostname%" с новым нумерованным паролем

--- config/_Scripts/AddUsers/CreateSharedAccount.cmd
+++ config/_Scripts/AddUsers/CreateSharedAccount.cmd
@@ -6,12 +6,16 @@ SETLOCAL ENABLEEXTENSIONS
 %SystemRoot%\System32\fltmc.exe >nul 2>&1 || ( ECHO Скрипт "%~f0" без прав администратора не работает & PING -n 30 127.0.0.1 >NUL & EXIT /B )
 
     CALL :SetOrAsk newUserName "Имя пользователя (логин)" "%~1"
-    CALL :SetOrAsk FullName "Полное имя (Назначение по-русски)" "%~2"
-    CALL :SetOrAsk Note "Описание (номер заявки / см. trello.com/c/ov1INOPm/15)" "%~3"
+    CALL :SetOrAsk FullName "Назначение учётной записи (Полное имя)" "%~2"
+    REM "Описание (номер заявки / см. trello.com/c/ov1INOPm/15)"
+    SET "Note=%~3" 
     IF NOT DEFINED Note SET "Note=Учетная запись создана %DATE% %TIME% пользователем %USERNAME%"
+    
+    SET "passwordFile=%~4"
+    IF NOT DEFINED passwordFile SET "passwordFile=NUL"
 )
 (
-    %SystemRoot%\System32\net.exe USER "%newUserName%" * /Add /FULLNAME:"%FullName%" /USERCOMMENT:"%Note%" /passwordchg:no /passwordreq:no || EXIT /B
+    TYPE "%passwordFile%"|%SystemRoot%\System32\net.exe USER "%newUserName%" * /Add /FULLNAME:"%FullName%" /USERCOMMENT:"%Note%" /passwordchg:no /passwordreq:no || EXIT /B
     %SystemRoot%\System32\wbem\wmic.exe path Win32_UserAccount where Name='%newUserName%' set PasswordExpires=false
     
     IF EXIST "D:\Users\*.*" CALL "%~dp0..\FindAutoHotkeyExe.cmd" "%~dp0..\MoveUserProfile\SetProfilesDirectory_D_Users.ahk"
@@ -20,9 +24,10 @@ EXIT /B
 
 :SetOrAsk <varName> <description> <value>
 (
+    IF NOT DEFINED Unattended IF "%RunInteractiveInstalls%"=="0" SET "Unattended=1"
     IF NOT "%~3"=="" (
 	SET "%~1=%~3"
-    ) ELSE IF NOT "%RunInteractiveInstalls%"=="0" (
+    ) ELSE IF NOT DEFINED Unattended (
 	SET /P "%~1=%~2: "
     ) ELSE EXIT /B 1
 EXIT /B
---
+++ config/_Scripts/AddUsers/CreateSharedAccount_user_Hostname.ahk
@@ -0,0 +1,39 @@
+﻿;by LogicDaemon <www.logicdaemon.ru>
+;This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
+#NoEnv
+
+EnvGet Unattended, Unattended
+If (!Unattended) {
+    EnvGet RunInteractiveInstalls, RunInteractiveInstalls
+    Unattended:= RunInteractiveInstalls=="0"
+}
+If (!Unattended && !A_IsAdmin) {
+    ScriptRunCommand:=DllCall( "GetCommandLine", "Str" )
+    Run *RunAs %ScriptRunCommand%,,UseErrorLevel  ; Requires v1.0.92.01+
+    ExitApp
+}
+
+AllowedChars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_@#$&*()[]{};'\:|,./?"
+
+passwd=
+Loop 14
+{
+    Random charNo, 1, % StrLen(AllowedChars)
+    passwd .= SubStr(AllowedChars,charNo,1)
+}
+
+Try {
+    passwdNo := WriteAndShowPassword(passwd, path_to_password_file := -1)
+} Catch e {
+;    If (e.What!="RecordPassword")
+;        MsgBox % "Exception " ObjectToText(e)
+;    ExitApp
+}
+RegRead Hostname, HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters, Hostname
+RunWait %comspec% /C ""%A_ScriptDir%\CreateSharedAccount.cmd" "user %Hostname%" "Пользователь компьютера %Hostname% без авторизации" "" "%path_to_password_file%""
+If (passwdNo)
+    Exit
+ExitApp
+
+#include %A_ScriptDir%\..\GUI\Input Numbered Passwd.ahk
+#include %A_ScriptDir%\..\Lib\ObjectToText.ahk
