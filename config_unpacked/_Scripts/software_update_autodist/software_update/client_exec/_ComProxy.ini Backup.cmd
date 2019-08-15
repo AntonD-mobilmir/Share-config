e7dad45158da74d1337da546f2d78a3957996477
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-08-15 17:33:48 +0300

    software_update/client_exec/_ComProxy.ini Backup.cmd: резервное копирование ComProxy.ini на случай повреждения при внезапной перезагрузке

---
+++ config_unpacked/_Scripts/software_update_autodist/software_update/client_exec/_ComProxy.ini Backup.cmd
@@ -0,0 +1,3 @@
+@(REM coding:CP866
+IF EXIST "%SystemDrive%\ComProxy\comproxy.ini" IF NOT EXIST "%SystemDrive%\ComProxy\comproxy.ini.bak" COPY /B "%SystemDrive%\ComProxy\comproxy.ini" "%SystemDrive%\ComProxy\comproxy.ini.bak" <NUL
+)
