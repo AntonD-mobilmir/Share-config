bed33c4d0e0c483a6f81ef028fa5e600c6c8f743
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-08-15 17:40:24 +0300

    CopyDistributives_*.cmd переделаны для использования robocopy с Srv1S-B вместо rsync

--- config/_Scripts/CopyDistributives_AllSoft.cmd
+++ config/_Scripts/CopyDistributives_AllSoft.cmd
@@ -1,17 +1,22 @@
-@REM coding:CP866
-@REM by LogicDaemon <www.logicdaemon.ru>
-@REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
+SETLOCAL ENABLEEXTENSIONS
 
-IF NOT DEFINED exe7z CALL "%~dp0find7zexe.cmd" || PAUSE
-
-SET localDist=D:\Distributives
-FOR /R %localDist% %%I IN (".sync*") DO (
-    IF "%%~nxI"==".sync" DEL "%%~I"
-    IF "%%~nxI"==".sync.includes" DEL "%%~I"
-    IF "%%~nxI"==".sync.excludes" DEL "%%~I"
+    IF NOT EXIST "D:\Distributives\Soft" MKDIR "D:\Distributives\Soft"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft" "D:\Distributives\Soft" /MIR /DCOPY:DAT /SL /XO /ETA /XD "\\Srv1S-B.office0.mobilmir\Distributives\Soft\AntiViruses AntiTrojans\Microsoft Security Essentials" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\Chat Messengers" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Office Text Publishing\Text Documents" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\PreInstalled\manual"
+EXIT /B
 )
+rem IF NOT DEFINED exe7z CALL "%~dp0find7zexe.cmd" || PAUSE
 
-%exe7z% x -aoa -y -o"%localDist%" -- "%~dpn0.syncmarker.7z"
-XCOPY "%~dp0rsync_DistributivesFromSrv0.cmd" "%localDist%" /Y /I
+rem SET localDist=D:\Distributives
+rem FOR /R %localDist% %%I IN (".sync*") DO (
+rem     IF "%%~nxI"==".sync" DEL "%%~I"
+rem     IF "%%~nxI"==".sync.includes" DEL "%%~I"
+rem     IF "%%~nxI"==".sync.excludes" DEL "%%~I"
+rem )
 
-START "Копирование дистрибутивов" /MIN /D "%localDist%" %comspec% /U /C "%localDist%\rsync_DistributivesFromSrv0.cmd"
+rem %exe7z% x -aoa -y -o"%localDist%" -- "%~dpn0.syncmarker.7z"
+rem XCOPY "%~dp0rsync_DistributivesFromSrv0.cmd" "%localDist%" /Y /I
+
+rem START "Копирование дистрибутивов" /MIN /D "%localDist%" %comspec% /U /C "%localDist%\rsync_DistributivesFromSrv0.cmd"
--- config/_Scripts/CopyDistributives_Downloaders.cmd
+++ config/_Scripts/CopyDistributives_Downloaders.cmd
@@ -1,17 +1,48 @@
-@REM coding:CP866
-@REM by LogicDaemon <www.logicdaemon.ru>
-@REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
+SETLOCAL ENABLEEXTENSIONS
 
-IF NOT DEFINED exe7z CALL "%~dp0find7zexe.cmd" || PAUSE
+    IF NOT EXIST "D:\Distributives\Soft\Archivers Packers\7Zip" MKDIR "D:\Distributives\Soft\Archivers Packers\7Zip"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Archivers Packers\7Zip" "D:\Distributives\Soft\Archivers Packers\7Zip" /MIR /DCOPY:DAT /SL /XO /ETA /XF *.exe
+    
+    IF NOT EXIST "D:\Distributives\Soft\Graphics\Viewers Managers\FreeVimager" MKDIR "D:\Distributives\Soft\Graphics\Viewers Managers\FreeVimager"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Graphics\Viewers Managers\FreeVimager" "D:\Distributives\Soft\Graphics\Viewers Managers\FreeVimager" /MIR /DCOPY:DAT /SL /XO /ETA /XF *.exe
+    
+    IF NOT EXIST "D:\Distributives\Soft\Keyboard Tools\AutoHotkey" MKDIR "D:\Distributives\Soft\Keyboard Tools\AutoHotkey"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Keyboard Tools\AutoHotkey" "D:\Distributives\Soft\Keyboard Tools\AutoHotkey" /MIR /DCOPY:DAT /SL /XO /ETA /XF *.exe /XD "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Keyboard Tools\AutoHotkey\Libs" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Keyboard Tools\AutoHotkey\old" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Keyboard Tools\AutoHotkey\Standalone" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Keyboard Tools\AutoHotkey\temp" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Keyboard Tools\AutoHotkey\Utils"
+    
+    IF NOT EXIST "D:\Distributives\Soft\Keyboard Tools\Punto Switcher" MKDIR "D:\Distributives\Soft\Keyboard Tools\Punto Switcher"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Keyboard Tools\Punto Switcher" "D:\Distributives\Soft\Keyboard Tools\Punto Switcher" /MIR /DCOPY:DAT /SL /XO /ETA /XF PuntoSwitcherSetup.exe "PuntoSwitcherSetup 3.1.1.exe" setup_ps295.exe
+    
+    IF NOT EXIST "D:\Distributives\Soft\MultiMedia\Players\foobar2000" MKDIR "D:\Distributives\Soft\MultiMedia\Players\foobar2000"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\MultiMedia\Players\foobar2000" "D:\Distributives\Soft\MultiMedia\Players\foobar2000" /MIR /DCOPY:DAT /SL /XO /ETA /XF *.exe
 
-SET localDist=D:\Distributives
-FOR /R %localDist% %%I IN (".sync*") DO (
-    IF "%%~nxI"==".sync" DEL "%%~I"
-    IF "%%~nxI"==".sync.includes" DEL "%%~I"
-    IF "%%~nxI"==".sync.excludes" DEL "%%~I"
+    IF NOT EXIST "D:\Distributives\Soft\Network\HTTP\Google Chrome" MKDIR "D:\Distributives\Soft\Network\HTTP\Google Chrome"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\HTTP\Google Chrome" "D:\Distributives\Soft\Network\HTTP\Google Chrome" /MIR /DCOPY:DAT /SL /XO /ETA /XF policy_templates.zip *.msi
+
+    IF NOT EXIST "D:\Distributives\Soft\Network\HTTP\Mozilla FireFox" MKDIR "D:\Distributives\Soft\Network\HTTP\Mozilla FireFox"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\HTTP\Mozilla FireFox" "D:\Distributives\Soft\Network\HTTP\Mozilla FireFox" /MIR /DCOPY:DAT /SL /XO /ETA /XF *.exe /XD "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\HTTP\Mozilla FireFox\temp" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\HTTP\Mozilla FireFox\latest-mozilla-central" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\HTTP\Mozilla FireFox\utilities" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\HTTP\Mozilla FireFox\W2K"
+
+    IF NOT EXIST "D:\Distributives\Soft\Network\Mail News\Mozilla Thunderbird" MKDIR "D:\Distributives\Soft\Network\Mail News\Mozilla Thunderbird"
+    %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\Mail News\Mozilla Thunderbird" "D:\Distributives\Soft\Network\Mail News\Mozilla Thunderbird" /MIR /DCOPY:DAT /SL /XO /ETA /XF *.exe /XD "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\Mail News\Mozilla Thunderbird\temp" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\Mail News\Mozilla Thunderbird\Extensions" "\\Srv1S-B.office0.mobilmir\Distributives\Soft\Network\Mail News\Mozilla Thunderbird\W2K"
+
+rem     IF NOT EXIST "D:\Distributives\Soft\^" MKDIR "D:\Distributives\Soft\^"
+rem     %SystemRoot%\System32\robocopy.exe "\\Srv1S-B.office0.mobilmir\Distributives\Soft\^" "D:\Distributives\Soft\^" /MIR /DCOPY:DAT /SL /XO /ETA /XF *.exe
+
+EXIT /B
 )
 
-%exe7z% x -aoa -y -o"%localDist%" -- "%~dpn0.syncmarker.7z"
-XCOPY "%~dp0rsync_DistributivesFromSrv0.cmd" "%localDist%" /Y /I
+rem IF NOT DEFINED exe7z CALL "%~dp0find7zexe.cmd" || PAUSE
 
-START "Копирование дистрибутивов" /MIN /D "%localDist%" %comspec% /U /C "%localDist%\rsync_DistributivesFromSrv0.cmd"
+rem SET localDist=D:\Distributives
+rem FOR /R %localDist% %%I IN (".sync*") DO (
+rem     IF "%%~nxI"==".sync" DEL "%%~I"
+rem     IF "%%~nxI"==".sync.includes" DEL "%%~I"
+rem     IF "%%~nxI"==".sync.excludes" DEL "%%~I"
+rem )
+
+rem %exe7z% x -aoa -y -o"%localDist%" -- "%~dpn0.syncmarker.7z"
+rem XCOPY "%~dp0rsync_DistributivesFromSrv0.cmd" "%localDist%" /Y /I
+
+rem START "Копирование дистрибутивов" /MIN /D "%localDist%" %comspec% /U /C "%localDist%\rsync_DistributivesFromSrv0.cmd"
