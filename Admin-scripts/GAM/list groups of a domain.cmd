6a99f39c09aa44797c08fe97f274929c8e844474
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-08-29 12:57:33 +0300

    Admin-scripts/GAM/list groups of a domain.cmd: если домен не указан, будут сохранены списки пользователей mobilmir.ru и status.mobilmir.ru

--- Admin-scripts/GAM/list groups of a domain.cmd
+++ Admin-scripts/GAM/list groups of a domain.cmd
@@ -3,10 +3,15 @@ REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
 SETLOCAL ENABLEEXTENSIONS
 
-    SET "domain=%~1"
-    IF NOT DEFINED domain SET "domain=mobilmir.ru"
+    SET "today=%DATE:~-4,4%-%DATE:~-7,2%-%DATE:~-10,2%"
 )
+(
+    IF "%~1"=="" FOR %%A IN ("mobilmir.ru" "status.mobilmir.ru" "zel.mobilmir.ru") DO CALL :PrintDomainGroups "%%~A" >"groups %%~A %today%.txt"
+EXIT /B
+)
+:PrintDomainGroups
 @(
-    CALL "%~dp0switchdomain.cmd" %domain%
-    CALL "%~dp0gam.cmd" print group-members > "groups %domain% %DATE:~-4,4%-%DATE:~-7,2%-%DATE:~-10,2%.txt"
+    CALL "%~dp0switchdomain.cmd" %1
+    CALL "%~dp0gam.cmd" print group-members
+EXIT /B
 )
