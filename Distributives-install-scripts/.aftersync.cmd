453de23c58f0b432276207037e33904edb5a5583
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-09-02 15:09:37 +0300

    путь для синхронизации скриптов изменен с Srv0 на Srv1S-B

--- Distributives-install-scripts/.aftersync.cmd
+++ Distributives-install-scripts/.aftersync.cmd
@@ -1,9 +1,8 @@
 @(REM coding:CP866
 REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
-IF NOT DEFINED xlnexe CALL "..\..\..\profiles$\Share\config\_Scripts\find_exe.cmd" xlnexe xln.exe c:\SysUtils\xln.exe "\\Srv0.office0.mobilmir\Distributives\Soft\PreInstalled\utils\xln.exe" || EXIT /B -1
-IF NOT DEFINED exe7z CALL "..\..\..\profiles$\Share\config\_Scripts\find7zexe.cmd" || EXIT /B -1
+    IF NOT DEFINED exe7z CALL "%~dp0..\..\..\profiles$\Share\config\_Scripts\find7zexe.cmd" || ( ECHO 7z.exe not found & EXIT /B -1 )
 )
 (
-%exe7z% x -aoa -o"%USERPROFILE%\Git\Share-config\Distributives_unpacked\Soft\PreInstalled\manual\TotalCommander.config" -x!"pci.db" -x!*.key -- "\\Srv0.office0.mobilmir\Distributives\Soft\PreInstalled\manual\TotalCommander.config.7z"
+    %exe7z% x -aoa -o"%USERPROFILE%\Git\Share-config\Distributives_unpacked\Soft\PreInstalled\manual\TotalCommander.config" -x!"pci.db" -x!*.key -- "\\Srv1S-B.office0.mobilmir\Distributives\Soft\PreInstalled\manual\TotalCommander.config.7z"
 )
