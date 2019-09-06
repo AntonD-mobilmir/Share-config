e3d6b9d82457add302ba4e90482f1cb7c5ebd2b3
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-09-06 17:38:08 +0300

    4th gen - Haswell/install.cmd: встроенная полноценная функция поиска Autohotkey.exe

--- Distributives-install-scripts/Drivers/Intel/Graphics/4th gen - Haswell/install.cmd
+++ Distributives-install-scripts/Drivers/Intel/Graphics/4th gen - Haswell/install.cmd
@@ -1,37 +1,29 @@
-(
-@REM coding:CP866
-SET "srcpath=%~dp0"
-SET "OSCapacity=32"
-IF /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" SET "OSCapacity=64"
-IF /I "%PROCESSOR_ARCHITEW6432%"=="AMD64" SET "OSCapacity=64"
-FOR /F "usebackq delims=" %%W IN (`ver`) DO SET "VW=%%~W"
+@(REM coding:CP866
+    SET "srcpath=%~dp0"
+    SET "OSCapacity=32"
+    IF /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" SET "OSCapacity=64"
+    IF /I "%PROCESSOR_ARCHITEW6432%"=="AMD64" SET "OSCapacity=64"
+    FOR /F "usebackq delims=" %%W IN (`ver`) DO SET "VW=%%~W"
+
+    IF NOT DEFINED AutohotkeyExe CALL :FindAutohotkeyExe || EXIT /B
+    CALL :InitRemembering
 )
-(
 IF "%VW:~0,22%"=="Microsoft Windows XP [" SET "OSCapacity=xp"
-
-FOR /F "usebackq tokens=2 delims==" %%I IN (`FTYPE AutoHotkeyScript`) DO CALL :GetFirstArg AutohotkeyExe %%I
-IF NOT DEFINED AutohotkeyExe CALL "\\Srv0.office0.mobilmir\profiles$\Share\config\_Scripts\FindAutoHotkeyExe.cmd" || EXIT /B
+(
+    FOR /R "%~dp0" %%I IN ("win%OSCapacity%*.zip") DO CALL :RememberIfLatest srczip "%%~fI"
+    FOR /R "%~dp0WU\%OSCapacity%-bit" %%I IN ("*.cab") DO CALL :RememberIfLatest srccab "%%~fI"
 )
 (
-CALL :InitRemembering
-FOR /R "%~dp0" %%I IN ("win%OSCapacity%*.zip") DO CALL :RememberIfLatest srczip "%%~fI"
-FOR /R "%~dp0WU\%OSCapacity%-bit" %%I IN ("*.cab") DO CALL :RememberIfLatest srccab "%%~fI"
+    IF DEFINED srccab (
+        START "" /B /WAIT %comspec% /C "%srcpath%WU\Install.cmd"
+    ) ELSE (
+        %AutohotkeyExe% "%srcpath%..\..\install Intel zip.ahk" "%srczip%"
+    )
+    CALL "%srcpath%..\RemoveFromStartupAndContextMenu.cmd"
+
+    EXIT /B
 )
 
-IF DEFINED srccab (
-    START "" /B /WAIT %comspec% /C "%srcpath%WU\Install.cmd"
-) ELSE (
-    %AutohotkeyExe% "%srcpath%..\..\install Intel zip.ahk" "%srczip%"
-)
-CALL "%srcpath%..\RemoveFromStartupAndContextMenu.cmd"
-
-EXIT /B
-
-:GetFirstArg
-(
-    SET %1=%2
-EXIT /B
-)
 :InitRemembering
 (
     SET "LatestDate=0000000000:00"
@@ -50,3 +42,59 @@ EXIT /B
 	SET "LatestDate=%CurrentDate%"
     )
 EXIT /B
+
+:FindAutohotkeyExe
+(
+    FOR /F "usebackq tokens=1* delims==" %%I IN (`ftype AutoHotkeyScript`) DO CALL :CheckAutohotkeyExe %%J
+    IF NOT DEFINED AutohotkeyExe GOTO :RunFindAutohotkeyExeScript || EXIT /B
+    IF "%~1"=="" EXIT /B
+)
+(
+    %AutohotkeyExe% %*
+    EXIT /B
+)
+:CheckAutohotkeyExe <path>
+(
+    IF NOT EXIST %1 EXIT /B 1
+    SET AutohotkeyExe=%1
+    EXIT /B
+)
+:RunFindAutohotkeyExeScript
+    IF NOT DEFINED configDir CALL :findConfigDir || EXIT /B
+(
+    IF DEFINED configDir CALL "%configDir%_Scripts\FindAutoHotkeyExe.cmd" %*
+EXIT /B
+)
+:findConfigDir
+(
+    IF NOT DEFINED DefaultsSource CALL "%ProgramData%\mobilmir.ru\_get_defaultconfig_source.cmd" || CALL "%SystemDrive%\Local_Scripts\_get_defaultconfig_source.cmd"
+
+    IF NOT DEFINED DefaultsSource CALL :FirstExisting DefaultsSource "D:\Distributives\config\." "%~d0\Distributives\config\." && GOTO :foundDefaultsSource
+    CALL :SplitNetPath host share "%~dp0"
+)
+(
+    CALL :FirstExisting DefaultsSource "\\%host%\profiles$\Share\config\." "\\%host%\Users\Public\Shares\profiles$\Share\config\."
+    IF NOT DEFINED DefaultsSource EXIT /B 1
+)
+:foundDefaultsSource
+(
+    CALL :GetDir configDir "%DefaultsSource%"
+EXIT /B
+)
+:GetDir
+(
+    SET "%~1=%~dp2"
+EXIT /B
+)
+
+:SplitNetPath <hostVar> <shareVar> <path>
+@(
+    FOR /F "delims=\ tokens=1,2" %%A IN ("%~3") DO (
+        IF "%%~A"=="" EXIT /B 1
+        IF "%%~B"=="" EXIT /B 2
+        SET "%~1=%%~A"
+        SET "%~2=%%~B"
+        EXIT /B
+    )
+EXIT /B 3
+)
