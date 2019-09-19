ebabdb1e8e49dea85d3907ef8163db82e0765a70
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-09-19 18:37:15 +0300

    дистрибутив LibreOffice 4.1.6 перенесен в Soft_old

--- Distributives-install-scripts/Soft/Office Text Publishing/Office Suites/LibreOffice 4.1.6/assoc_csvtsv_with_LO.cmd
+++
@@ -1,5 +0,0 @@
-@REM coding:OEM
-ASSOC .tsv=LibreOffice.PlainTextSpreadsheet
-ASSOC .csv=LibreOffice.PlainTextSpreadsheet
-
-FOR /F "usebackq tokens=2 delims==" %%I IN (`ftype LibreOffice.CalcDocument.1`) DO FTYPE LibreOffice.PlainTextSpreadsheet=%%I
