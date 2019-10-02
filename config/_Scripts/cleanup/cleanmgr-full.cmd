c039a42434c6defaafdbccd0e405d68e61be072c
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-10-02 12:19:49 +0300

    cleanmgr-full.cmd будет выносить папку Users, если такая есть в Windows.old, чтобы избежать автоматического удаления старых профилей пользователе после переустановки Windows *без сохранения данных*

--- config/_Scripts/cleanup/cleanmgr-full.cmd
+++ config/_Scripts/cleanup/cleanmgr-full.cmd
@@ -1,10 +1,12 @@
-@REM coding:OEM
+@REM coding:CP866
 IF DEFINED PROCESSOR_ARCHITEW6432 (
     "%SystemRoot%\SysNative\cmd.exe" /C %0 %*
     EXIT /B
 )
 
 rem cleanmgr.exe /sageset:65535
+
+FOR %%A IN (C D E F G H I J K L M N O P Q R S T U V W X Y Z) DO IF EXIST "%%~A:\Windows.old\Users" CALL :MoveUsers "%%~A:\Windows.old"
 
 CALL "%~dp0..\CheckWinVer.cmd" 10 && GOTO :Win10
 CALL "%~dp0..\CheckWinVer.cmd" 6 && GOTO :WinVista
@@ -30,3 +32,13 @@ START "mousemove while cleanmgr.exe running.ahk" /LOW /MIN %comspec% /C ""%~dp0.
 
 cleanmgr.exe /sagerun:65535
 EXIT /B
+
+:MoveUsers
+@SET "date=%~t1"
+@SET "date=%date::=%"
+@SET "date=%date:\=%"
+@SET "date=%date:/=%"
+(
+    MOVE "%~1\Users" "%~dp1Users @ %~nx1 %date%"
+EXIT /B
+)
