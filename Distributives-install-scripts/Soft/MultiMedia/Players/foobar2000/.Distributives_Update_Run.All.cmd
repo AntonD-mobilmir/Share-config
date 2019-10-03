db2611b9ee892f1ae0c80fafb5ba751ab4215646
AntonD-mobilmir <anticode+github@it.mobilmir.ru>
2019-10-03 10:01:39 +0300

    foobar2000 удален из списка стандартного ПО

--- Distributives-install-scripts/Soft/MultiMedia/Players/foobar2000/.Distributives_Update_Run.All.cmd
+++
@@ -1,5 +0,0 @@
-@(REM coding:CP866
-    SETLOCAL ENABLEEXTENSIONS
-    SET "AddtoS_UScripts=1"
-    CALL "%~dp0download.cmd" %*
-)
--- config/_Scripts/cleanup/uninstall/350 foobar2000.cmd
+++ config/_Scripts/cleanup/uninstall/350 foobar2000.cmd
@@ -2,8 +2,10 @@
 REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <http://creativecommons.org/licenses/by-sa/4.0/deed.ru>.
 IF NOT DEFINED ProgramFiles32 CALL "%~dp0Lib\_init.cmd"
+
 )
-IF EXIST "%ProgramFiles32%\foobar2000\foobar2000.exe" IF EXIST "%SoftSourceDir%\MultiMedia\Players\foobar2000\*.exe" (
+IF EXIST "%ProgramFiles32%\foobar2000\foobar2000.exe" (
+    rem IF EXIST "%SoftSourceDir%\..\Soft com freeware\MultiMedia\Players\foobar2000\*.exe" 
     CALL "%~dp0..\..\Lib\.utils.cmd" MarkForInstall "%SoftSourceDir%\MultiMedia\Players\foobar2000\install.cmd"
     ECHO %DATE% %TIME% Удаление foobar2000
     %SystemRoot%\System32\taskkill.exe /F /IM foobar2000.exe
